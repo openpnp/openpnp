1bfc1142c9fb23a646479e476e20b6194b980438
Jason von Nieda <jason@vonnieda.org>
2013-04-26 00:02:57 -0700

    Came up with a good binding system for location fields. Need to apply it everywhere now.

--- gui/src/main/java/org/openpnp/gui/wizards/CameraConfigurationWizard.java
+++ gui/src/main/java/org/openpnp/gui/wizards/CameraConfigurationWizard.java
@@ -34,13 +34,17 @@ import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
+import org.jdesktop.beansbinding.AutoBinding.UpdateStrategy;
 import org.openpnp.gui.MainFrame;
 import org.openpnp.gui.components.CameraView;
 import org.openpnp.gui.components.ComponentDecorators;
 import org.openpnp.gui.support.AbstractConfigurationWizard;
 import org.openpnp.gui.support.LengthConverter;
 import org.openpnp.gui.support.MessageBoxes;
+import org.openpnp.model.AbstractModelObject;
 import org.openpnp.model.Configuration;
+import org.openpnp.model.Length;
+import org.openpnp.model.Location;
 import org.openpnp.spi.Camera;
 
 import com.jgoodies.forms.factories.FormFactory;
@@ -110,11 +114,90 @@ public class CameraConfigurationWizard extends AbstractConfigurationWizard {
 	public void createBindings() {
 		LengthConverter lengthConverter = new LengthConverter(Configuration.get());
 		
-		addWrappedBinding(camera, "unitsPerPixel.lengthX", textFieldUppX, "text", lengthConverter);
-		addWrappedBinding(camera, "unitsPerPixel.lengthY", textFieldUppY, "text", lengthConverter);
+		MutableLocationProxy unitsPerPixel = new MutableLocationProxy();
+        bind(UpdateStrategy.READ_WRITE, camera, "unitsPerPixel", unitsPerPixel, "location");
+        addWrappedBinding(unitsPerPixel, "lengthX", textFieldUppX, "text", lengthConverter);
+        addWrappedBinding(unitsPerPixel, "lengthY", textFieldUppY, "text", lengthConverter);
 		
 		ComponentDecorators.decorateWithAutoSelectAndLengthConversion(textFieldUppX);
 		ComponentDecorators.decorateWithAutoSelectAndLengthConversion(textFieldUppY);
+	}
+	
+	/**
+	 * A proxy class that allows bindings to mutate a Location field by field
+	 * by replacing the bound Location whenever a field is changed. 
+	 */
+	static public class MutableLocationProxy extends AbstractModelObject {
+	    private Location location;
+	    
+	    public Location getLocation() {
+	        return location;
+	    }
+	    
+	    public void setLocation(Location location) {
+	        this.location = location;
+	        firePropertyChange("location", null, getLocation());
+	    }
+	    
+        public void setLengthX(Length l) {
+            if (l.getUnits() != location.getUnits()) {
+                location = location.convertToUnits(l.getUnits());
+                location = location.derive(l.getValue(), null, null, null);
+                firePropertyChange("lengthX", null, getLengthX());
+                firePropertyChange("lengthY", null, getLengthY());
+                firePropertyChange("lengthZ", null, getLengthZ());
+                firePropertyChange("location", null, getLocation());
+            }
+            else {
+                location = location.derive(l.getValue(), null, null, null);
+                firePropertyChange("lengthX", null, getLengthX());
+                firePropertyChange("location", null, getLocation());
+            }
+        }
+        
+        public void setLengthY(Length l) {
+            if (l.getUnits() != location.getUnits()) {
+                location = location.convertToUnits(l.getUnits());
+                location = location.derive(null, l.getValue(), null, null);
+                firePropertyChange("lengthX", null, getLengthX());
+                firePropertyChange("lengthY", null, getLengthY());
+                firePropertyChange("lengthZ", null, getLengthZ());
+                firePropertyChange("location", null, getLocation());
+            }
+            else {
+                location = location.derive(null, l.getValue(), null, null);
+                firePropertyChange("lengthY", null, getLengthY());
+                firePropertyChange("location", null, getLocation());
+            }
+        }
+        
+        public void setLengthZ(Length l) {
+            if (l.getUnits() != location.getUnits()) {
+                location = location.convertToUnits(l.getUnits());
+                location = location.derive(null, null, l.getValue(), null);
+                firePropertyChange("lengthX", null, getLengthX());
+                firePropertyChange("lengthY", null, getLengthY());
+                firePropertyChange("lengthZ", null, getLengthZ());
+                firePropertyChange("location", null, getLocation());
+            }
+            else {
+                location = location.derive(null, null, l.getValue(), null);
+                firePropertyChange("lengthZ", null, getLengthY());
+                firePropertyChange("location", null, getLocation());
+            }
+        }
+        
+        public Length getLengthX() {
+            return location.getLengthX();
+        }
+        
+        public Length getLengthY() {
+            return location.getLengthY();
+        }
+        
+        public Length getLengthZ() {
+            return location.getLengthZ();
+        }
 	}
 
 	private Action measureAction = new AbstractAction("Measure") {
