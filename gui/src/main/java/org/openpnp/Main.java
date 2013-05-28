a9d428399b1d8146ddbfb85d25ad8aeb2568a466
Karl Lew <karl@firepick.org>
2013-05-27 18:36:31 -0700

    1) Renamed log4j.properties as log4j.properties.template so that openpnp will properly use log4j.properties in user.home folder even if openpnp-gui.jar is used by other artifacts. Without this change, the log4j.properties used for defaulting is in the classpath, with disables the user.home initialization. 2) Changed Main so that the slf4j intialization via LoggerFactory in Main happens AFTER log4j.properties has been properly defaulted on startup.

--- gui/src/main/java/org/openpnp/Main.java
+++ gui/src/main/java/org/openpnp/Main.java
@@ -21,78 +21,82 @@
 
 package org.openpnp;
 
-import java.awt.EventQueue;
-import java.io.File;
-
-import javax.swing.UIManager;
-
 import org.apache.commons.io.FileUtils;
 import org.openpnp.gui.MainFrame;
 import org.openpnp.model.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
 /**
  * Start with -Xdock:name=OpenPnP on Mac to make it prettier.
- * @author jason
  *
+ * @author jason
  */
 public class Main {
-	private static final Logger logger = LoggerFactory.getLogger(Main.class);
-	
-	public static String getVersion() {
-		String version = Main.class.getPackage().getImplementationVersion();
-		if (version == null) {
-			version = "INTERNAL BUILD";
-		}
-		return version;
-	}
-	
-	public static void main(String[] args) {
-		// http://developer.apple.com/library/mac/#documentation/Java/Conceptual/Java14Development/07-NativePlatformIntegration/NativePlatformIntegration.html#//apple_ref/doc/uid/TP40001909-212952-TPXREF134
-		System.setProperty("apple.laf.useScreenMenuBar", "true");
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		}
-		catch (Exception e) {
-			throw new Error(e);
-		}
-		
-		File configurationDirectory = new File(System.getProperty("user.home"));
-		configurationDirectory = new File(configurationDirectory, ".openpnp");
-		
-		// If the log4j.properties is not in the configuration directory, copy
-		// the default over.
-		File log4jConfigurationFile = new File(configurationDirectory, "log4j.properties");
-		if (!log4jConfigurationFile.exists()) {
-			try {
-				FileUtils.copyURLToFile(ClassLoader.getSystemResource("log4j.properties"), log4jConfigurationFile);
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		// Use the local configuration if it exists.
-		if (log4jConfigurationFile.exists()) {
-			System.setProperty("log4j.configuration", log4jConfigurationFile.toURI().toString());
-		}
-		
-		logger.debug(String.format("OpenPnP %s Started.", Main.getVersion()));
-		
-		Configuration.initialize(configurationDirectory);
-		final Configuration configuration = Configuration.get();
-		final JobProcessor jobProcessor = new JobProcessor(configuration);
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainFrame frame = new MainFrame(configuration, jobProcessor);
-					frame.setVisible(true);
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    private static Logger logger; // See initalizeLog4JEnvironment
+
+    public static String getVersion() {
+        String version = Main.class.getPackage().getImplementationVersion();
+        if (version == null) {
+            version = "INTERNAL BUILD";
+        }
+        return version;
+    }
+
+    public static void main(String[] args) {
+        // http://developer.apple.com/library/mac/#documentation/Java/Conceptual/Java14Development/07-NativePlatformIntegration/NativePlatformIntegration.html#//apple_ref/doc/uid/TP40001909-212952-TPXREF134
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+
+        File configurationDirectory = new File(System.getProperty("user.home"));
+        configurationDirectory = new File(configurationDirectory, ".openpnp");
+
+        initializeLog4JEnvironment(configurationDirectory);
+
+        logger = LoggerFactory.getLogger(Main.class);
+        logger.debug(String.format("OpenPnP %s Started.", Main.getVersion()));
+
+        Configuration.initialize(configurationDirectory);
+        final Configuration configuration = Configuration.get();
+        final JobProcessor jobProcessor = new JobProcessor(configuration);
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainFrame frame = new MainFrame(configuration, jobProcessor);
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * IMPORTANT! Call this BEFORE ANY LoggerFactory call.
+     * If the log4j.properties is not in the configuration directory, copy
+     * the default from log4j.properties.template.
+     */
+    private static void initializeLog4JEnvironment(File configurationDirectory) {
+        File log4jConfigurationFile = new File(configurationDirectory, "log4j.properties");
+        if (!log4jConfigurationFile.exists()) {
+            try {
+                FileUtils.copyURLToFile(ClassLoader.getSystemResource("log4j.properties.template"), log4jConfigurationFile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Use the local configuration if it exists.
+        if (log4jConfigurationFile.exists()) {
+            System.setProperty("log4j.configuration", log4jConfigurationFile.toURI().toString());
+        }
+    }
 }
