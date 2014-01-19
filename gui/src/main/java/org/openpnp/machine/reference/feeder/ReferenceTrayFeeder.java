96727cca57edbffb231e1dadf59d252b92e470dc
Jason von Nieda <jason@vonnieda.org>
2014-01-19 14:02:38 -0800

    Fixes an offset bug in ReferenceTrayFeeder. Thanks rspelling!

--- gui/src/main/java/org/openpnp/machine/reference/feeder/ReferenceTrayFeeder.java
+++ gui/src/main/java/org/openpnp/machine/reference/feeder/ReferenceTrayFeeder.java
@@ -88,10 +88,9 @@ public class ReferenceTrayFeeder extends ReferenceFeeder {
         }
         
         // Multiply the offsets by the X/Y part indexes to get the total offsets
-        // and then add the pickLocation to offset the final value.
-        pickLocation = offsets
-                .multiply(partX, partY, 0.0, 0.0)
-                .add(location);
+        // and then add them to the location to get the final pickLocation.
+        pickLocation = location.add(
+                offsets.multiply(partX, partY, 0.0, 0.0));
 
         logger.debug(String.format(
                 "Feeding part # %d, x %d, y %d, xPos %f, yPos %f", feedCount,
