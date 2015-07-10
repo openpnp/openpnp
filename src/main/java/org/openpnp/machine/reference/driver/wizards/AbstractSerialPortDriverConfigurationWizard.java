16f2d1c5ab8269c377370f33caa92d12d459cf82
Jason von Nieda <jason@vonnieda.org>
2015-07-09 23:17:19 -0700

    Updated wizard to check null to avoid exception when working in WindowBuilder.

--- src/main/java/org/openpnp/machine/reference/driver/wizards/AbstractSerialPortDriverConfigurationWizard.java
+++ src/main/java/org/openpnp/machine/reference/driver/wizards/AbstractSerialPortDriverConfigurationWizard.java
@@ -72,16 +72,18 @@ public class AbstractSerialPortDriverConfigurationWizard extends AbstractConfigu
         comboBoxBaud.addItem(new Integer(460800));
         comboBoxBaud.addItem(new Integer(921600));
         
-        boolean exists = false;
-        String[] portNames = driver.getPortNames();
-        for (String portName : portNames) {
-            comboBoxPort.addItem(portName);
-            if (portName.equals(driver.getPortName())) {
-                exists = true;
+        if (driver != null) {
+            boolean exists = false;
+            String[] portNames = driver.getPortNames();
+            for (String portName : portNames) {
+                comboBoxPort.addItem(portName);
+                if (portName.equals(driver.getPortName())) {
+                    exists = true;
+                }
             }
-        }
-        if (!exists && driver.getPortName() != null) {
-            comboBoxPort.addItem(driver.getPortName());
+            if (!exists && driver.getPortName() != null) {
+                comboBoxPort.addItem(driver.getPortName());
+            }
         }
     }
     
