dcd9b17a44f3506de2f06f82427498724e2132be
Jason von Nieda <jason@vonnieda.org>
2015-09-27 16:21:56 +0800

    Add a method to get kbits per second for the mjpeg streamer. Add a comment about how to improve streaming performance.

--- src/main/java/org/openpnp/web/MjpegCameraResponse.java
+++ src/main/java/org/openpnp/web/MjpegCameraResponse.java
@@ -15,11 +15,18 @@ import lombok.Getter;
 import org.openpnp.spi.Camera;
 import org.openpnp.util.OpenCvUtils;
 
+// TODO: We really only want to be encoding a given frame once. Instead of
+// each connection getting it's own encoder and FPS we should just pick
+// a FPS, use a static table of cameras to encoders and then stream
+// frames from that encoder per connection.
+
 @Getter
 class MjpegCameraResponse implements StreamingOutput {
     private String boundary = "camera_mjpeg";
     private Camera camera;
     private int framesPerSecond;
+    private long bytesWritten;
+    private long timeStarted;
     
     public MjpegCameraResponse(Camera camera, int framesPerSecond) {
         this.camera = camera;
@@ -37,10 +44,18 @@ class MjpegCameraResponse implements StreamingOutput {
                 .build();
     }
     
+    public double getKbitsPerSecond() {
+        double seconds = (System.currentTimeMillis() - timeStarted) / 1000;
+        double kBits = (bytesWritten * 8 / 1024.0);
+        double kBitsPerSecond = kBits / seconds;
+        return kBitsPerSecond;
+    }
+    
     @Override
     public void write(OutputStream out) throws IOException,
             WebApplicationException {
         out.write(("--" + boundary + "\n").getBytes());
+        timeStarted = System.currentTimeMillis();
         while (true) {
             BufferedImage image = camera.capture();
             image = OpenCvUtils.convertBufferedImage(image, BufferedImage.TYPE_INT_RGB);
@@ -55,6 +70,7 @@ class MjpegCameraResponse implements StreamingOutput {
             out.write(bytes);
             out.write(("\n--" + boundary + "\n").getBytes());
             out.flush();
+            bytesWritten += bytes.length;
             
             try {
                 Thread.sleep(1000 / framesPerSecond);
