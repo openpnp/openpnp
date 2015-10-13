f60b4944f3de205869fec40bcc2f93d1a3874269
Jason von Nieda <jason@vonnieda.org>
2015-10-13 11:03:21 +0800

    Added ransac and some experiments for better line fitting. Still work to do but way more promising than the previous method.

--- src/main/java/org/openpnp/vision/FluentCv.java
+++ src/main/java/org/openpnp/vision/FluentCv.java
@@ -7,8 +7,13 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import javax.imageio.ImageIO;
 
@@ -34,6 +39,10 @@ import org.openpnp.util.VisionUtils;
  * 
  *  Heavily influenced by FireSight by Karl Lew
  *  https://github.com/firepick1/FireSight
+ *  
+ *  TODO: Rethink operations that return or process data points versus
+ *  images. Perhaps these should require a tag to work with and
+ *  leave the image unchanged.
  */
 public class FluentCv {
     static {
@@ -131,6 +140,17 @@ public class FluentCv {
 		return this;
 	}
 	
+	public FluentCv circlesToPoints(List<Point> points) {
+    	for (int i = 0; i < mat.cols(); i++) {
+    		double[] circle = mat.get(0, i);
+    		double x = circle[0];
+    		double y = circle[1];
+    		double radius = circle[2];
+    		points.add(new Point(x, y));
+    	}
+    	return this;
+	}
+	
 	public FluentCv circlesToLocations(List<Location> locations) {
 		checkCamera();
     	Location unitsPerPixel = camera
@@ -170,21 +190,13 @@ public class FluentCv {
         			mat, 
         			new Point(x, y), 
         			(int) radius, 
-        			new Scalar(
-        					color.getBlue(), 
-        					color.getGreen(), 
-        					color.getRed(), 
-        					255), 
+        			colorToScalar(color), 
         			2);
         	Core.circle(
         			mat, 
         			new Point(x, y), 
         			1, 
-        			new Scalar(
-        					centerColor.getBlue(), 
-        					centerColor.getGreen(), 
-        					centerColor.getRed(), 
-        					255), 
+        			colorToScalar(centerColor), 
         			2);
     	}
 		return store(mat, tag);
@@ -307,8 +319,8 @@ public class FluentCv {
 	 */
 	public FluentCv filterCirclesToLine(double maxDistance, String... tag) {
 		// fitLine doesn't like when you give it a zero length array, so just
-		// bail if that's the case.
-		if (this.mat.cols() == 0) {
+		// bail if there are not enough points to make a line.
+		if (this.mat.cols() < 2) {
 			return store(this.mat, tag);
 		}
 		
@@ -321,10 +333,11 @@ public class FluentCv {
     		float y = circle[1];
     		points.add(new Point(x, y));
     	}
+    	
+    	long t = System.currentTimeMillis();
+		// match the points to a line
     	MatOfPoint pointsMat = new MatOfPoint();
 		pointsMat.fromList(points);
-		
-		// match the points to a line
 		Mat line = new Mat();
 		Imgproc.fitLine(
 				pointsMat, 
@@ -339,6 +352,14 @@ public class FluentCv {
 		float y = (float) line.get(3, 0)[0];
 		Point a = new Point(x, y);
 		Point b = new Point(x + vx, y + vy);
+		System.out.println(System.currentTimeMillis() - t);
+		
+    	t = System.currentTimeMillis();
+		Point[] lineRansac = ransac(points, 100, maxDistance);
+    	a = lineRansac[0];
+    	b = lineRansac[1];
+		System.out.println(System.currentTimeMillis() - t);
+		System.out.println();
 		
     	// filter the points by distance from the resulting line
 		List<float[]> results = new ArrayList<float[]>();
@@ -391,6 +412,14 @@ public class FluentCv {
 		return mat.clone();
 	}
 	
+	private static Scalar colorToScalar(Color color) {
+		return new Scalar(
+				color.getBlue(), 
+				color.getGreen(), 
+				color.getRed(), 
+				255);
+	}
+	
     private static BufferedImage convertBufferedImage(BufferedImage src, int type) {
         if (src.getType() == type) {
             return src;
@@ -409,20 +438,159 @@ public class FluentCv {
 		return Math.abs((P.x - A.x) * (B.y - A.y) - (P.y - A.y) * (B.x - A.x)) / normalLength;
 	}
 	
+	/**
+	 * Draw the infinite line defined by the two points to the extents
+	 * of the image instead of just between the two points.
+	 * From: http://stackoverflow.com/questions/13160722/how-to-draw-line-not-line-segment-opencv-2-4-2
+	 * @param img
+	 * @param p1
+	 * @param p2
+	 * @param color
+	 */
+	private static void infiniteLine(Mat img, Point p1, Point p2, Color color) {
+		Point p = new Point(), q = new Point();
+		// Check if the line is a vertical line because vertical lines don't
+		// have slope
+		if (p1.x != p2.x) {
+			p.x = 0;
+			q.x = img.cols();
+			// Slope equation (y1 - y2) / (x1 - x2)
+			float m = (float) ((p1.y - p2.y) / (p1.x - p2.x));
+			// Line equation: y = mx + b
+			float b = (float) (p1.y - (m * p1.x));
+			p.y = m * p.x + b;
+			q.y = m * q.x + b;
+		} else {
+			p.x = q.x = p2.x;
+			p.y = 0;
+			q.y = img.rows();
+		}
+		Core.line(img, p, q, colorToScalar(color));
+	}
+
+	/*
+	 * http://users.utcluj.ro/~igiosan/Resources/PRS/L1/lab_01e.pdf
+	 * http://cs.gmu.edu/~kosecka/cs682/lect-fitting.pdf
+	 * http://introcs.cs.princeton.edu/java/36inheritance/LeastSquares.java.html
+	 */
+	public static Point[] ransac(List<Point> points, int maxIterations, double threshold) {
+		Point bestA = null, bestB = null;
+		int bestInliers = 0;
+		for (int i = 0; i < maxIterations; i++) {
+			// take a random sample of two points
+			Collections.shuffle(points);
+			Point a = points.get(0);
+			Point b = points.get(1);
+			// find the inliers
+			int inliers = 0;
+			for (Point p : points) {
+				double distance = pointToLineDistance(a, b, p);
+				if (distance <= threshold) {
+					inliers++;
+				}
+			}
+			if (inliers > bestInliers) {
+				bestA = a;
+				bestB = b;
+				bestInliers = inliers;
+			}
+		}
+		return new Point[] { bestA, bestB };
+	}
+	
+	// TODO: This currently seems to give much worse results than ransac. Figure out why.
+	public static List<RansacLine> multiRansac(List<Point> points, int maxIterations, double threshold) {
+		Random random = new Random();
+		Set<RansacLine> lines = new HashSet<>();
+		for (int i = 0; i < maxIterations; i++) {
+			// take a random sample of two points
+			Point a = points.get(random.nextInt(points.size()));
+			Point b = points.get(random.nextInt(points.size()));
+			RansacLine line = new RansacLine(a, b, 0);
+			// if we have already processed this pair, skip it
+			if (lines.contains(line)) {
+				continue;
+			}
+			// add the result
+			lines.add(line);
+			// find the inliers
+			for (Point p : points) {
+				double distance = pointToLineDistance(a, b, p);
+				if (distance <= threshold) {
+					line.inliers++;
+				}
+			}
+		}
+		List<RansacLine> results = new ArrayList<>(lines);
+		Collections.sort(results, new Comparator<RansacLine>() {
+			@Override
+			public int compare(RansacLine o1, RansacLine o2) {
+				return o2.inliers - o1.inliers;
+			}
+		});
+		return results;
+	}
+	
+	public static class RansacLine {
+		public Point a;
+		public Point b;
+		public transient int inliers;
+		
+		public RansacLine(Point a, Point b, int inliers) {
+			this.a = a;
+			this.b = b;
+			this.inliers = inliers;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((a == null) ? 0 : a.hashCode());
+			result = prime * result + ((b == null) ? 0 : b.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			RansacLine other = (RansacLine) obj;
+			if (a == null) {
+				if (other.a != null)
+					return false;
+			} else if (!a.equals(other.a))
+				return false;
+			if (b == null) {
+				if (other.b != null)
+					return false;
+			} else if (!b.equals(other.b))
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "RansacLine [a=" + a + ", b=" + b + ", inliers=" + inliers + "]";
+		}
+	}
+	
     public static void main(String[] args) throws Exception {
-		new FluentCv()
-			.read(new File("/Users/jason/Desktop/dots.png"), "original")
+    	List<Point> points = new ArrayList<>();
+		FluentCv cv = new FluentCv()
+			.read(new File("/Users/jason/Desktop/test.png"), "original")
 			.toGray()
-			.thresholdOtsu(false)
 			.gaussianBlur(9)
 			.houghCircles(28, 32, 10, "hough")
 			.drawCircles("original", Color.red, "unfiltered")
 			.recall("hough")
-			.filterCirclesByDistance(200, 200, 0, 150)
-			.drawCircles("unfiltered", Color.blue, "distanceFiltered")
-			.recall("hough")
 			.filterCirclesToLine(10)
-			.drawCircles("distanceFiltered", Color.green)
+			.circlesToPoints(points)
+			.drawCircles("unfiltered", Color.green)
 			.write(new File("/Users/jason/Desktop/test_out.png"));
     }
     
