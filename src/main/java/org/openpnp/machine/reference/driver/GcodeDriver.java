3f723adfa730562dcd5cdee5998826cf70f49974
Jason von Nieda <jason@vonnieda.org>
2016-04-01 22:30:58 -0700

    Fixes formatting issue for non-US locales in variable substitutions in GcodeDriver.

--- src/main/java/org/openpnp/machine/reference/driver/GcodeDriver.java
+++ src/main/java/org/openpnp/machine/reference/driver/GcodeDriver.java
@@ -3,6 +3,7 @@ package org.openpnp.machine.reference.driver;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -399,7 +400,7 @@ public class GcodeDriver extends AbstractSerialPortDriver implements Runnable {
             }
             String v = "";
             if (value != null) {
-                v = String.format(format, value);
+                v = String.format((Locale) null, format, value);
             }
             matcher.appendReplacement(sb, v);
         }
