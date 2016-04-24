653027577977fc537a1fc16276c37ac18b3eb69e
Jason von Nieda <jason@vonnieda.org>
2016-04-23 22:32:44 -0700

    Removes the obsolete FirePick driver. Fixes #262.

--- src/main/java/org/openpnp/machine/reference/wizards/ReferenceMachineConfigurationWizard.java
+++ src/main/java/org/openpnp/machine/reference/wizards/ReferenceMachineConfigurationWizard.java
@@ -50,8 +50,6 @@ public class ReferenceMachineConfigurationWizard extends AbstractConfigurationWi
         comboBoxDriver.addItem(SprinterDriver.class.getCanonicalName());
         comboBoxDriver.addItem(TinygDriver.class.getCanonicalName());
         comboBoxDriver.addItem(OpenBuildsDriver.class.getCanonicalName());
-        comboBoxDriver.addItem(org.firepick.driver.MarlinDriver.class.getCanonicalName());
-        comboBoxDriver.addItem(org.firepick.driver.FireStepDriver.class.getCanonicalName());
 
         this.driverClassName = machine.getDriver().getClass().getCanonicalName();
     }
