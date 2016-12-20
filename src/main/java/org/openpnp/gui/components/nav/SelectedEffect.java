29d3f95be85b22499175cf052c2398fc38bb71a0
Jason von Nieda <jason@vonnieda.org>
2016-12-19 20:26:15 -0600

    Nav panel no longer requires -DenableNav. Instead it detects if JavaFX is installed and automatically shows it if so.

--- src/main/java/org/openpnp/gui/components/nav/SelectedEffect.java
+++ src/main/java/org/openpnp/gui/components/nav/SelectedEffect.java
@@ -1,10 +1,11 @@
 package org.openpnp.gui.components.nav;
 
+import javafx.scene.effect.BlurType;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.paint.Color;
 
 public class SelectedEffect extends DropShadow {
     public SelectedEffect() {
-        super(7, Color.BLACK);
+        super(BlurType.GAUSSIAN, Color.BLACK, 2, 0.65, 0, 0);
     }
 }
