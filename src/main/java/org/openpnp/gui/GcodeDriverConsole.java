774e4b0f8c40742f10ba27ddf8c222aa3dd31766
dzach <dzach@users.noreply.github.com>
2017-02-01 17:48:11 +0200

    Added history to Gcode console.

--- src/main/java/org/openpnp/gui/GcodeDriverConsole.java
+++ src/main/java/org/openpnp/gui/GcodeDriverConsole.java
@@ -35,13 +35,6 @@ import org.openpnp.machine.reference.driver.GcodeDriver.Command;
 import org.openpnp.machine.reference.driver.GcodeDriver.CommandType;
 import org.openpnp.model.Configuration;
 import org.openpnp.model.LengthUnit;
-import org.openpnp.spi.Actuator;
-import org.openpnp.spi.Camera;
-import org.openpnp.spi.Head;
-import org.openpnp.spi.HeadMountable;
-import org.openpnp.spi.Nozzle;
-import org.openpnp.spi.PasteDispenser;
-import org.simpleframework.xml.Serializer;
 
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
@@ -57,6 +50,11 @@ public class GcodeDriverConsole extends AbstractConfigurationWizard {
 
     public GcodeDriverConsole(GcodeDriver driver) {
         this.driver = driver;
+        
+        historyLen = 0;
+        historyMaxLen = 50;
+        history = new String[historyMaxLen];
+        historyCursor = 0;
         
         JPanel gcodeConsole = new JPanel();
         gcodeConsole.setBorder(
@@ -95,19 +93,43 @@ public class GcodeDriverConsole extends AbstractConfigurationWizard {
         lblCmdLine = new JLabel("Command line:");
         gcodeConsole.add(lblCmdLine, "2, 4");
         
-        cmdLineTextField = new JTextField();
+        cmdLineTextField = new JTextField();              Logger.debug("keyCode = " + e.getKeyCode() + ", historyCursor = " + historyCursor);
+
         gcodeConsole.add(cmdLineTextField,"4, 4");
+        
         cmdLineTextField.addKeyListener(new KeyAdapter() {
             @Override
-            public void keyTyped(KeyEvent e) {
-                if (e.getKeyChar() == '\n') {
-                  sendGcodeConCmd();
-                }
-                else {
-                    super.keyTyped(e);
-                }
+            public void keyPressed(KeyEvent e) {
+            
+              switch (e.getKeyCode()) {
+                case 0:  return;
+                
+                case 38: // arrow key UP, older history
+                  if (historyCursor < historyLen - 1) {
+                    historyCursor++;
+                  }
+                  cmdLineTextField.setText(history[historyCursor]);
+                  break;
+                  
+                case 40:  // arrow key DOWN, more recent history
+                  if (historyCursor > 0) {
+                    historyCursor--;
+                  }
+                  cmdLineTextField.setText(history[historyCursor]);
+                  break;
+                  
+                default:
+                  if (e.getKeyChar() == '\n') {
+                    sendGcodeConCmd();
+                  }
+                  else {
+                      super.keyTyped(e);
+                  }
+                  break;
+              }
             }
-        });
+          }
+        );
 
         sendGcodeConCmdBtn = new JButton(sendGcodeConCmdAction);
         gcodeConsole.add(sendGcodeConCmdBtn, "6, 4");
@@ -125,9 +147,40 @@ public class GcodeDriverConsole extends AbstractConfigurationWizard {
     private JTextField cmdLineTextField;
     private JButton sendGcodeConCmdBtn;
     
+    private String history[];
+    private int historyLen;
+    private int historyMaxLen;
+    private int historyCursor;
+        
+    private String getHistoryAtIdx() {
+      return history[historyCursor];
+    }
+    
     private void sendGcodeConCmd() {
-      String cmd = cmdLineTextField.getText().toUpperCase();
+    
+      String cmd = cmdLineTextField.getText();
+      int moveLen = 0;
+      
+      // are we repeating the same command? Store it in history only if different than last one
+      if (!cmd.equals(history[0])) {
+        if (historyLen < historyMaxLen) {
+          moveLen = historyLen++;
+        } else {
+          moveLen = historyMaxLen - 1;
+        }
+        // shift history one place to the past. Copy is done in-place.
+        System.arraycopy(history, 0, history, 1, moveLen);
+        // store current command at 0
+        history[0] = cmd;
+      }
+      // reset historyCursor to the present
+      historyCursor = 0;
+      
+      // most controllers prefer commands to be in upper case.
+      cmd = cmd.toUpperCase();
+      // display the command in the console
       textAreaConsole.append("> " + cmd + "\n");
+      // Send command and get responses
       try {
         for(String line: driver.sendCommand(cmd,5000)) {
           textAreaConsole.append(line + "\n");
@@ -136,7 +189,6 @@ public class GcodeDriverConsole extends AbstractConfigurationWizard {
       catch (Exception ex) {
         Logger.debug("Gcode console error: " + ex);
       }
-    
     }
     
     private Action sendGcodeConCmdAction = new AbstractAction("Send") {
