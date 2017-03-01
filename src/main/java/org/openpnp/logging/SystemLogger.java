47932eff9afee5c46ab18512a4984fa6a8b426e1
Friedrich Mäckle <maeckle@nantis.de>
2017-03-01 13:53:49 +0100

    Adding a SystemLogger class for redirecting stdout and stderr to tinylog
    Class was named SystemLogger because tinylog uses reflection and the class name will be used for tagging the LogEntry
    
     Unfortunately there is no generic log function where one could pass a LogLevel, therefore it is used in the constructor of the SystemLogger to be evaluated on logging.

---
+++ src/main/java/org/openpnp/logging/SystemLogger.java
@@ -0,0 +1,48 @@
+package org.openpnp.logging;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.pmw.tinylog.Level;
+import org.pmw.tinylog.Logger;
+
+/**
+ * A SystemLogger class that redirects another stream like e.g. stdout or stderr to the tinylog logger
+ */
+public class SystemLogger extends PrintStream {
+
+    private static final String lineSeparator = System.getProperty("line.separator");
+
+    private Level logLevel;
+
+    public SystemLogger(OutputStream out, Level logLevel) {
+        super(out);
+        this.logLevel = logLevel;
+    }
+
+    public SystemLogger(OutputStream out) {
+        super(out);
+        this.logLevel = Level.INFO;
+    }
+
+    @Override
+    public void write(byte[] buf, int off, int len) {
+        super.write(buf, off, len);
+
+        byte[] pb = new byte[len];
+        System.arraycopy(buf, off, pb, 0, len);
+        String str = new String(pb);
+
+        if(!str.equals(lineSeparator)) {
+            // There is no generic log function where one could pass the log level
+            switch (logLevel) {
+                case INFO:
+                    Logger.info(str);
+                    break;
+                case ERROR:
+                    Logger.error(str);
+                    break;
+            }
+        }
+    }
+}
