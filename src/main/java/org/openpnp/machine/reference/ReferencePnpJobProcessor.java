52acb35990cee109a24abb312127fe9a9bda7aa0
Roland Piirsoo <roland.piirsoo@gmail.com>
2017-03-29 13:05:40 +0200

    Fix code formatting

--- src/main/java/org/openpnp/machine/reference/ReferencePnpJobProcessor.java
+++ src/main/java/org/openpnp/machine/reference/ReferencePnpJobProcessor.java
@@ -798,24 +798,24 @@ public class ReferencePnpJobProcessor extends AbstractPnpJobProcessor {
     }
 
     public static List<JobPlacement> getJobPlacementsById(String id) {      
-        return jobPlacements.stream().filter((jobPlacement) -> { 
-          return jobPlacement.toString() == id; 
-        }).collect(Collectors.toList()); 
-      } 
+		return jobPlacements.stream().filter((jobPlacement) -> { 
+		  return jobPlacement.toString() == id; 
+		}).collect(Collectors.toList()); 
+    } 
        
-      public static List<JobPlacement> getProcessingJobPlacementsById(String id) { 
-        return jobPlacements.stream().filter((jobPlacement) -> { 
-          return jobPlacement.status == Status.Processing && 
-              jobPlacement.toString() == id; 
-        }).collect(Collectors.toList()); 
-      } 
+	public static List<JobPlacement> getProcessingJobPlacementsById(String id) { 
+		return jobPlacements.stream().filter((jobPlacement) -> { 
+		  return jobPlacement.status == Status.Processing && 
+		      jobPlacement.toString() == id; 
+		}).collect(Collectors.toList()); 
+	} 
        
-      public static List<JobPlacement> getCompleteJobPlacementsById(String id) {      
-        return jobPlacements.stream().filter((jobPlacement) -> { 
-          return jobPlacement.status == Status.Complete && 
-              jobPlacement.toString() == id; 
-        }).collect(Collectors.toList()); 
-      } 
+	public static List<JobPlacement> getCompleteJobPlacementsById(String id) {      
+		return jobPlacements.stream().filter((jobPlacement) -> { 
+		  return jobPlacement.status == Status.Complete && 
+		      jobPlacement.toString() == id; 
+		}).collect(Collectors.toList()); 
+	} 
 
     // Sort a List<JobPlacement> by the number of nulls it contains in ascending order.
     Comparator<List<JobPlacement>> byFewestNulls = (a, b) -> {
