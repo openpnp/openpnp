2d123bd8bf431a0a98fbb4fd6dc402d917f54202
SadMan <git@sadman.net>
2017-04-05 18:49:16 +0200

    added some basic keyboard shortcuts

--- src/main/java/org/openpnp/gui/JobPanel.java
+++ src/main/java/org/openpnp/gui/JobPanel.java
@@ -25,7 +25,9 @@ import java.awt.Component;
 import java.awt.FileDialog;
 import java.awt.Frame;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
@@ -51,6 +53,7 @@ import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EtchedBorder;
@@ -350,6 +353,7 @@ public class JobPanel extends JPanel {
         add(splitPane);
 
         mnOpenRecent = new JMenu("Open Recent Job...");
+        mnOpenRecent.setMnemonic(KeyEvent.VK_R);
         loadRecentJobs();
 
         Configuration.get().addListener(new ConfigurationListener.Adapter() {
@@ -735,6 +739,12 @@ public class JobPanel extends JPanel {
     }
 
     public final Action openJobAction = new AbstractAction("Open Job...") {
+        {
+            putValue(MNEMONIC_KEY, KeyEvent.VK_O);
+            putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke('O',
+                    Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+        }
+
         @Override
         public void actionPerformed(ActionEvent arg0) {
             if (!checkJobStopped()) {
@@ -768,6 +778,12 @@ public class JobPanel extends JPanel {
     };
 
     public final Action newJobAction = new AbstractAction("New Job") {
+        {
+            putValue(MNEMONIC_KEY, KeyEvent.VK_N);
+            putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke('N',
+                    Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+        }
+
         @Override
         public void actionPerformed(ActionEvent arg0) {
             if (!checkJobStopped()) {
@@ -781,6 +797,12 @@ public class JobPanel extends JPanel {
     };
 
     public final Action saveJobAction = new AbstractAction("Save Job") {
+        {
+            putValue(MNEMONIC_KEY, KeyEvent.VK_S);
+            putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke('S',
+                    Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+        }
+
         @Override
         public void actionPerformed(ActionEvent arg0) {
             saveJob();
@@ -788,6 +810,10 @@ public class JobPanel extends JPanel {
     };
 
     public final Action saveJobAsAction = new AbstractAction("Save Job As...") {
+        {
+            putValue(MNEMONIC_KEY, KeyEvent.VK_A);
+        }
+
         @Override
         public void actionPerformed(ActionEvent arg0) {
             saveJobAs();
@@ -997,6 +1023,7 @@ public class JobPanel extends JPanel {
             putValue(NAME, "Add Board...");
             putValue(SMALL_ICON, Icons.add);
             putValue(SHORT_DESCRIPTION, "Add a new or existing board to the job.");
+            putValue(MNEMONIC_KEY, KeyEvent.VK_A);
         }
 
         @Override
@@ -1007,6 +1034,7 @@ public class JobPanel extends JPanel {
         {
             putValue(NAME, "New Board...");
             putValue(SHORT_DESCRIPTION, "Create a new board and add it to the job.");
+            putValue(MNEMONIC_KEY, KeyEvent.VK_N);
         }
 
         @Override
@@ -1048,6 +1076,7 @@ public class JobPanel extends JPanel {
         {
             putValue(NAME, "Existing Board...");
             putValue(SHORT_DESCRIPTION, "Add an existing board to the job.");
+            putValue(MNEMONIC_KEY, KeyEvent.VK_E);
         }
 
         @Override
@@ -1087,6 +1116,7 @@ public class JobPanel extends JPanel {
             putValue(SMALL_ICON, Icons.delete);
             putValue(NAME, "Remove Board");
             putValue(SHORT_DESCRIPTION, "Remove the selected board from the job.");
+            putValue(MNEMONIC_KEY, KeyEvent.VK_R);
         }
 
         @Override
