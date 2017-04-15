77c7509f018582352e0bd6928475dbaaccbe3fd5
Jason von Nieda <jason@vonnieda.org>
2017-04-15 12:13:19 -0500

    Remove example scripts that are not working, or that don't use submitMachineTask interface. Move script examples into Python and JavaScript subdirectories.

--- src/main/resources/scripts/Examples/Tool_Changer_All_Nozzle_Tips.py
+++
@@ -1,7 +0,0 @@
-# Get the nozzle
-my_nozzle=machine.defaultHead.defaultNozzle
-
-# Run load command for each nozzle tip
-for nt in my_nozzle.getNozzleTips():
-  print "###  %r  %s" % (nt.name, "#"*16),
-  my_nozzle.loadNozzleTip(nt),
