4f74cb4715277f3a8e7a7020d554cf58f4eeaf4f
dzach <dzach@users.noreply.github.com>
2017-06-18 16:50:52 +0300

    Added ability to create a template for rect parts from package body

--- src/main/java/org/openpnp/vision/pipeline/stages/ReadTemplateImage.java
+++ src/main/java/org/openpnp/vision/pipeline/stages/ReadTemplateImage.java
@@ -7,6 +7,8 @@ import java.io.File;
 
 import org.opencv.core.*;
 import org.opencv.highgui.Highgui;
+
+import org.openpnp.spi.Camera;
 import org.openpnp.vision.pipeline.CvPipeline;
 import org.openpnp.vision.pipeline.CvStage;
 import org.openpnp.vision.pipeline.Property;
@@ -55,6 +57,8 @@ public class ReadTemplateImage extends CvStage {
       File file = null;
       String filepath = templateFile;
       String filename = null;
+      double width = 0;
+      double height = 0;
       /**
       * Read a template image from the path set by the user.
       * If the path ends in 'extension' then the image is read directly from the path
@@ -79,16 +83,46 @@ public class ReadTemplateImage extends CvStage {
           filename = filepath + pipeline.getFeeder().getPart().getPackage().getId() + extension;
           file = new File(filename);
           if (!file.exists()) {
-            return null;
+            // If package body dimensions are set, use them as a template. 
+            // As is, useful only for non-polarized rectangular parts
+            // TODO: it would be best if we could define a package outline, e.g. as a polygon
+            // and use that to draw the part and match templates
+            if (pipeline.getFeeder().getPart().getPackage().getFootprint() != null) {
+              width = pipeline.getFeeder().getPart().getPackage().getFootprint().getBodyWidth();
+              height = pipeline.getFeeder().getPart().getPackage().getFootprint().getBodyHeight();
+              if (width == 0 || height == 0) {
+                // can't create 0 sized template
+                return null;
+              }
+              // portrait mode
+              if (width > height) {
+                double tmp = width;
+                width = height;
+                height = tmp;
+              }
+              // get length conversion value from camera
+              Camera camera = pipeline.getCamera();
+              width /= camera.getUnitsPerPixel().getX();
+              height /= camera.getUnitsPerPixel().getY();
+              // create a white rect image as the template
+              Mat templateImage = new Mat((int)height, (int)width, CvType.CV_8UC3);
+              templateImage.setTo(new Scalar(255,255,255));
+              // create a model
+              RotatedRect rrect = new RotatedRect(new Point(width/2, height/2), new Size(width, height), 0.0);
+              // that's all we can do for now
+              return new Result(templateImage,rrect);
+            } else {
+              return null;
+            }
           }
         }
       }
       // Read template image from disk
       Mat templateImage = Highgui.imread(file.getAbsolutePath());
 
-      int width = (int) templateImage.size().width;
-      int height = (int) templateImage.size().height;
-      if (width == 0 && height == 0) {
+      width = templateImage.size().width;
+      height = templateImage.size().height;
+      if (width == 0.0 && height == 0.0) {
         return null;
       }
       return new Result(templateImage, new RotatedRect(new Point(width / 2, height/2), new Size(width, height), (double) 0));
