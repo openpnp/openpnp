9eac780fc40f59c7942e6f4e81eca18ba7dd1168
cri-s <cri-s@users.noreply.github.com>
2017-06-21 02:34:20 +0200

    Update ClosedLoop.java

--- vision/pipeline/stages/ClosedLoop.java
+++ vision/pipeline/stages/ClosedLoop.java
@@ -1,16 +1,1 @@
-/*
- * © 2017 Cri.S<phone.cri@gmail.com> ALL RIGHTS RESERVED
- */
-package org.openpnp.vision.pipeline.stages;
-
-import org.openpnp.vision.pipeline.CvPipeline;
-import org.openpnp.vision.pipeline.CvStage;
-
-public class ClosedLoop
-extends CvStage {
-    @Override
-    public CvStage.Result process(CvPipeline pipeline) throws Exception {
-        return null;
-    }
-}
 
