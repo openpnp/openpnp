d5149c8b83d9d894919216f5d7ab1729c10ff9a2
cri-s <cri-s@users.noreply.github.com>
2017-06-26 02:40:06 +0200

    Update ReferenceBottomVision.java
    sorry, it's late here and i want/need to finish this PR

--- src/main/java/org/openpnp/machine/reference/vision/ReferenceBottomVision.java
+++ src/main/java/org/openpnp/machine/reference/vision/ReferenceBottomVision.java
@@ -110,8 +110,8 @@ public class ReferenceBottomVision implements PartAlignment {
 
 			rect = (RotatedRect) pipeline.getResult("result").model;
                         Logger.debug("Result rect {}", rect);
-		        String s = "Align offset for "+part.getName()+": " + offsets.toString() + "     ";
 			Location offsets = VisionUtils.getPixelCenterOffsets(pipeline.getCamera(), rect.center.x, rect.center.y).derive(null,null,null,Double.NaN);
+		        String s = "Align offset for "+part.getName()+": " + offsets.toString() + "     ";
                         MainFrame.get().getCameraViews().getCameraView(pipeline.getCamera()).showFilteredImage(OpenCvUtils.toBufferedImage(pipeline.getWorkingImage()),  s , 1500);
 			return new PartAlignment.PartAlignmentOffset(offsets, true);
         }
