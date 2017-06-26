78d533c36eec2d011726c40370679a79eb429593
cri-s <cri-s@users.noreply.github.com>
2017-06-26 15:44:34 +0200

    Update ReferenceBottomVision.java
    shorted it a bit, maybe this is more clearer to read

--- src/main/java/org/openpnp/machine/reference/vision/ReferenceBottomVision.java
+++ src/main/java/org/openpnp/machine/reference/vision/ReferenceBottomVision.java
@@ -54,6 +54,12 @@ public class ReferenceBottomVision implements PartAlignment {
     @ElementMap(required = false)
     protected Map<String, PartSettings> partSettingsByPartId = new HashMap<>();
 
+	private double angleNorm(double val, double lim)  { double clip = lim*2;
+			while (Math.abs(val) > lim) { nozzleAngle= (val += (val < 0.)? clip : -clip); }
+            return val;
+    } 
+	private double angleNorm(double val)  { return angleNorm(val,45.); }
+	
 
     @Override
     public PartAlignmentOffset findOffsets(Part part, BoardLocation boardLocation,
@@ -69,8 +75,6 @@ public class ReferenceBottomVision implements PartAlignment {
         // Pre-rotate to minimize runout
         double preRotateAngle = 0;
         if (preRotate) {
-		// Prerotate just prerotate nozzle, take picture, adjust rotation and take second picture before getting xy offsets.
-		// is is complicated by the fact that it need some workaround for rotation angle that is not normalized.
 			if(part == null || nozzle.getPart() == null) { throw new Exception("no part on nozzle"); }			
 			if((!part.toString().equals(nozzle.getPart().toString()))) { throw new Exception("Part mismatch with part on nozzle"); }
 			part=nozzle.getPart();
@@ -78,7 +82,7 @@ public class ReferenceBottomVision implements PartAlignment {
 			if(boardLocation!=null) {
 				angle = Utils2D.calculateBoardPlacementLocation(boardLocation, placementLocation).getRotation();              
 			}
-			while (Math.abs(angle) > 180.0) { angle += (angle < 0.)? 360. : -360.; } 
+			angle=angleNorm(angle,180.);
 			double nozzleAngle=angle;
 			double placementAngle=angle;
                         MovableUtils.moveToLocationAtSafeZ(nozzle, VisionUtils.getBottomVisionCamera().getLocation().add(new Location(part.getHeight().getUnits(), 0.0, 0.0,part.getHeight().getValue(), 0.0)).derive(null, null, null, angle));
@@ -90,25 +94,17 @@ public class ReferenceBottomVision implements PartAlignment {
 				throw new Exception("Bottom vision alignment failed for part " + part.getId()
 						+ " on nozzle " + nozzle.getName() + ". No result found.");
 			}
-		// first picture was ok, now calc error
-
 
 			RotatedRect rect = ((RotatedRect)(pipeline.getResult("result")).model);
-			if (rect.size.width < rect.size.height) { rect.angle = 90 + rect.angle; }
-			while (Math.abs(angle) > 45.0) { nozzleAngle= (angle += (angle < 0.)? 90 : -90); } 
-			while (Math.abs(rect.angle) > 45.0) { rect.angle += (rect.angle < 0)? 90 : -90; } 
-			angle += rect.angle;
-			while (Math.abs(angle) > 45.0) { angle += (angle < 0.)? 90 : -90; } 
+			angle = angleNorm(angleNorm(angle) + angleNorm((rect.size.width < rect.size.height) ? 90 + rect.angle : rect.angle));
 		// error is -angle
 			if(Math.abs(angle)>0.0765) { angle+=0.0567 * Math.signum(angle); } // rounding 
-		// error 
 			nozzle.moveTo(new Location(LengthUnit.Millimeters,Double.NaN, Double.NaN, Double.NaN, placementAngle+angle), part.getSpeed());
 			pipeline.process();
 			if (!((pipeline.getResult("result")).model instanceof RotatedRect)) {
 				throw new Exception("Bottom vision alignment failed for part " + part.getId()
 					+ " on nozzle " + nozzle.getName() + ". No result found.");
 			}
-		// second picture taken
 
 			rect = (RotatedRect) pipeline.getResult("result").model;
                         Logger.debug("Result rect {}", rect);
