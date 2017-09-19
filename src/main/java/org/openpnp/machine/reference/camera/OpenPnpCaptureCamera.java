0e660badc5978fe85c1e679c8f2736d2f11e3fd1
Jason von Nieda <jason@vonnieda.org>
2017-09-18 22:14:54 -0500

    capture camera implementation working, see TODOs

--- src/main/java/org/openpnp/machine/reference/camera/OpenPnpCaptureCamera.java
+++ src/main/java/org/openpnp/machine/reference/camera/OpenPnpCaptureCamera.java
@@ -20,19 +20,31 @@
 package org.openpnp.machine.reference.camera;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.List;
 
+import org.openpnp.CameraListener;
 import org.openpnp.capture.CaptureDevice;
+import org.openpnp.capture.CaptureDevice.CaptureFormat;
+import org.openpnp.capture.CaptureDevice.CaptureStream;
 import org.openpnp.capture.OpenPnpCapture;
 import org.openpnp.gui.support.Wizard;
 import org.openpnp.machine.reference.ReferenceCamera;
 import org.openpnp.machine.reference.camera.wizards.OpenPnpCaptureCameraConfigurationWizard;
 import org.openpnp.spi.PropertySheetHolder;
+import org.pmw.tinylog.Logger;
 
-public class OpenPnpCaptureCamera extends ReferenceCamera {
+// TODO: Color is wrong (BGR vs. RGB)
+// TODO: Objects get re-created each time wizard is loaded, so the dropdowns don't stick
+// TODO: Move the capture helper objects into this project. Just leave the bindings to just
+// the bindings and maybe some tests.
+public class OpenPnpCaptureCamera extends ReferenceCamera implements Runnable {
     private OpenPnpCapture capture = new OpenPnpCapture();
     private Thread thread;
-    private boolean dirty = false;
+    
+    private CaptureDevice device;
+    private CaptureFormat format;
+    private CaptureStream stream;
 
     public OpenPnpCaptureCamera() {
         
@@ -44,9 +56,117 @@ public class OpenPnpCaptureCamera extends ReferenceCamera {
 
     @Override
     public synchronized BufferedImage internalCapture() {
-        return null;
+        if (thread == null) {
+            open();
+        }
+        try {
+            BufferedImage img = stream.capture();
+            return transformImage(img);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
+    @Override
+    public synchronized void startContinuousCapture(CameraListener listener, int maximumFps) {
+        if (thread == null) {
+            open();
+        }
+        super.startContinuousCapture(listener, maximumFps);
+    }
+
+    public void run() {
+        while (!Thread.interrupted()) {
+            try {
+                BufferedImage image = internalCapture();
+                if (image != null) {
+                    broadcastCapture(image);
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void open() {
+        if (thread != null) {
+            thread.interrupt();
+            try {
+                thread.join(3000);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            thread = null;
+        }
+        
+        if (stream != null) {
+            try {
+                stream.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        stream = null;
+        
+        if (device == null) {
+            Logger.debug("open called with null device");
+            return;
+        }
+        if (format == null) {
+            Logger.debug("open called with null format");
+            return;
+        }
+        
+        
+        try {
+            width = null;
+            height = null;
+            
+            stream = device.openStream(format);
+            
+            System.out.println(stream);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        thread = new Thread(this);
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    @Override
+    public void close() throws IOException {
+        super.close();
+        
+        if (thread != null) {
+            thread.interrupt();
+            try {
+                thread.join(3000);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        if (stream != null) {
+            try {
+                stream.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        stream = null;
+
+        capture.close();
+    }
+    
     @Override
     public Wizard getConfigurationWizard() {
         return new OpenPnpCaptureCameraConfigurationWizard(this);
@@ -60,5 +180,23 @@ public class OpenPnpCaptureCamera extends ReferenceCamera {
     @Override
     public PropertySheetHolder[] getChildPropertySheetHolders() {
         return null;
-    }    
+    }
+
+    public CaptureDevice getDevice() {
+        return device;
+    }
+
+    public void setDevice(CaptureDevice device) {
+        Logger.debug("setDevice({})", device);
+        this.device = device;
+    }
+
+    public CaptureFormat getFormat() {
+        return format;
+    }
+
+    public void setFormat(CaptureFormat format) {
+        Logger.debug("setFormat({})", format);
+        this.format = format;
+    }
 }
--- src/main/java/org/openpnp/machine/reference/camera/wizards/OpenPnpCaptureCameraConfigurationWizard.java
+++ src/main/java/org/openpnp/machine/reference/camera/wizards/OpenPnpCaptureCameraConfigurationWizard.java
@@ -23,10 +23,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 
 import org.openpnp.capture.CaptureDevice;
+import org.openpnp.capture.CaptureDevice.CaptureFormat;
 import org.openpnp.machine.reference.camera.OpenCvCamera.OpenCvCapturePropertyValue;
 import org.openpnp.machine.reference.camera.OpenPnpCaptureCamera;
 import org.openpnp.machine.reference.wizards.ReferenceCameraConfigurationWizard;
@@ -36,40 +38,75 @@ import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.FormSpecs;
 import com.jgoodies.forms.layout.RowSpec;
 
-/*
- * TODO: newly added properties don't get set on first apply
- */
 @SuppressWarnings("serial")
 public class OpenPnpCaptureCameraConfigurationWizard extends ReferenceCameraConfigurationWizard {
     private final OpenPnpCaptureCamera camera;
-    
+
     private List<OpenCvCapturePropertyValue> properties = new ArrayList<>();
     private JPanel panel;
-    private JComboBox comboBox;
-    
+    private JComboBox deviceCb;
+    private JLabel lblDevice;
+    private JLabel lblFormat;
+    private JComboBox formatCb;
+
     public OpenPnpCaptureCameraConfigurationWizard(OpenPnpCaptureCamera camera) {
         super(camera);
 
         this.camera = camera;
         createUi();
     }
+
     private void createUi() {
-        
+
         panel = new JPanel();
-        panel.setBorder(new TitledBorder(null, "General", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        panel.setBorder(new TitledBorder(null, "General", TitledBorder.LEADING, TitledBorder.TOP,
+                null, null));
         contentPanel.add(panel);
-        panel.setLayout(new FormLayout(new ColumnSpec[] {
-                FormSpecs.RELATED_GAP_COLSPEC,
-                ColumnSpec.decode("default:grow"),},
-            new RowSpec[] {
-                FormSpecs.RELATED_GAP_ROWSPEC,
-                FormSpecs.DEFAULT_ROWSPEC,}));
-        
-        comboBox = new JComboBox();
-        panel.add(comboBox, "2, 2, fill, default");
+        panel.setLayout(new FormLayout(
+                new ColumnSpec[] {FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
+                        FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("default:grow"),},
+                new RowSpec[] {FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+                        FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,}));
+
+        lblDevice = new JLabel("Device");
+        panel.add(lblDevice, "2, 2, right, default");
+
+        deviceCb = new JComboBox();
+        panel.add(deviceCb, "4, 2");
+
+        lblFormat = new JLabel("Format");
+        panel.add(lblFormat, "2, 4, right, default");
+
+        formatCb = new JComboBox();
+        panel.add(formatCb, "4, 4, fill, default");
+
+        deviceCb.addActionListener(l -> {
+            formatCb.removeAllItems();
+            CaptureDevice dev = (CaptureDevice) deviceCb.getSelectedItem();
+            if (dev == null) {
+                return;
+            }
+            for (CaptureFormat format : dev.getFormats()) {
+                formatCb.addItem(format);
+            }
+        });
 
         for (CaptureDevice dev : camera.getCaptureDevices()) {
-            comboBox.addItem(dev);
+            deviceCb.addItem(dev);
         }
+    }
+    
+    @Override
+    public void createBindings() {
+        super.createBindings();
+        
+        addWrappedBinding(camera, "device", deviceCb, "selectedItem");
+        addWrappedBinding(camera, "format", formatCb, "selectedItem");
+    }
+
+    @Override
+    protected void saveToModel() {
+        super.saveToModel();
+        camera.open();
     }
 }
