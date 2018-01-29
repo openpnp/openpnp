e4a287d4c823535ed1baf7aebd717337287ff583
johnthagen <johnthagen@gmail.com>
2018-01-29 11:17:57 -0500

    Make scripts Python 3 compatible by using __future__ imports

--- src/main/resources/scripts/Examples/Python/Print_Methods_Vars.py
+++ src/main/resources/scripts/Examples/Python/Print_Methods_Vars.py
@@ -1,14 +1,15 @@
+from __future__ import print_function
 import sys
 
 # Prints all methods & variables and values of defaultNozzle
 def print_methods_vars(element, clip=True):
-  print " ",
+  print(" ", end=' ')
   for item in dir(element):
     try:
       s1="%s.%s = %s" % (element.name, item, eval("element.%s" % item))
       if clip: s1=s1[:78]
-      print s1,
+      print(s1, end=' ')
     except:
-      print "WARNING ... for", item,
+      print("WARNING ... for", item, end=' ')
 
 print_methods_vars(machine.defaultHead.defaultNozzle)
--- src/main/resources/scripts/Examples/Python/Print_Nozzle_Info.py
+++ src/main/resources/scripts/Examples/Python/Print_Nozzle_Info.py
@@ -1,8 +1,10 @@
+from __future__ import print_function
+
 # Get the nozzle
 my_nozzle=machine.defaultHead.defaultNozzle
 
 # Run print information regarding nozzle
-print "###  Name &  Location of %r  %s" % (my_nozzle.name, "#"*16),
-print my_nozzle,
-print my_nozzle.name,
-print my_nozzle.location
+print("###  Name &  Location of %r  %s" % (my_nozzle.name, "#"*16), end=' ')
+print(my_nozzle, end=' ')
+print(my_nozzle.name, end=' ')
+print(my_nozzle.location)
