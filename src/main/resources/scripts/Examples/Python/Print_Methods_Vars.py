ad4453404ff44316652652a67230cac452692297
johnthagen <johnthagen@gmail.com>
2018-01-31 08:58:41 -0500

    Make methods info script Python 3 compatible and update module name to be PEP8 compliant

--- src/main/resources/scripts/Examples/Python/Print_Methods_Vars.py
+++ src/main/resources/scripts/Examples/Python/Print_Methods_Vars.py
@@ -1,14 +1,13 @@
-import sys
-
-# Prints all methods & variables and values of defaultNozzle
+# Prints all methods & variables and values of an element.
 def print_methods_vars(element, clip=True):
-  print " ",
-  for item in dir(element):
-    try:
-      s1="%s.%s = %s" % (element.name, item, eval("element.%s" % item))
-      if clip: s1=s1[:78]
-      print s1,
-    except:
-      print "WARNING ... for", item,
+    for item in dir(element):
+        try:
+            s1 = '{}.{} = {}'.format(element.name, item, eval('element.{}'.format(item)))
+            if clip:
+                s1 = s1[:78]
+            print(s1)
+        except TypeError as e:
+            print('WARNING ... for {}: {}'.format(item, e))
+
 
 print_methods_vars(machine.defaultHead.defaultNozzle)
