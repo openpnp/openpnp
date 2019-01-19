3d2f8cfd26d9392c5a45509fc4792734b9c48ce7
mgrl <michael@groene.de>
2019-01-19 09:43:00 +0100

    remove isHomed flag on start of homing process (in case of rehoming this is needed)

--- src/main/java/org/openpnp/spi/base/AbstractMachine.java
+++ src/main/java/org/openpnp/spi/base/AbstractMachine.java
@@ -177,6 +177,9 @@ public abstract class AbstractMachine extends AbstractModelObject implements Mac
 
     @Override
     public void home() throws Exception {
+        // if one rehomes, the isHomed flag has to be removed
+        this.setHomed(false);
+        
         for (Head head : heads) {
             head.home();
         }
