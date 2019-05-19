openpnp/openpnp

commit 676f80b5b138d9ad55dfb37e61a64a9ecff9ec84

OpenBuildsDriver, GcodeDriver and LinuxCNC disconnect when disabled.
This allows for a serial port to be reused in another program (i.e. Pronterface) or unplugged, or the controller reset without closing OpenPNP. If the serial port becomes available again, the machine can be enabled again and the connection succeeds.

Before this change this failed at least under Windows where a COM port name is blocked until explicitly closed by the process. Even resetting/re-plugging the controller will not free the port and also not reestablished the connection. OpenPNP has to be closed and the controller reset/re-plugged one more time, before a freshly started OpenPNP can reconnect.

When closing OpenPNP the user loses the GUI navigation which is very annoying when editing Gcode fragments. Along with Gcode editing, users may need to change config.txt on Smoothieware controllers, which requires a reset of the controller. Which again (at least on Windows) was only possible after exiting OpenPNP.

src/main/java/org/openpnp/machine/reference/driver/LinuxCNC.java
@@ -215,6 +215,9 @@ public class LinuxCNC implements ReferenceDriver, Runnable {
         if (connected) {
             sendCommand("set machine " + (enabled ? "on" : "off"));
         }
+        if (connected && !enabled) {
+            disconnect();
+        }
     }
 
     public synchronized void connect(String serverIp, int port) throws Exception {

src/main/java/org/openpnp/machine/openbuilds/OpenBuildsDriver.java
@@ -72,6 +72,9 @@ public class OpenBuildsDriver extends AbstractReferenceDriver implements Runnabl
                 pump(false);
             }
         }
+        if (connected && !enabled) {
+            disconnect();
+        }
     }
 
     @Override

src/main/java/org/openpnp/machine/reference/driver/GcodeDriver.java
@@ -271,6 +271,9 @@ public class GcodeDriver extends AbstractReferenceDriver implements Named, Runna
         for (ReferenceDriver driver : subDrivers) {
             driver.setEnabled(enabled);
         }
+        if (connected && !enabled) {
+            disconnect();
+        }
     }
 
     @Override

commit 033195cb70137495e031846ece14fac3df486e61

Fixes a regression in the LinuxCNC driver that makes it impossible to connect. The bug was introduced in 676f80b5b138d9ad55dfb37e61a64a9ecff9ec84, which did not take into consideration that the connect() method itself calls setEnabled(false) after setting connected = true. The bug was introduced in the OpenBuilds driver but since that one is scheduled for removal and it can be mitigated by setting connection keep alive I'm not fixing it.

src/main/java/org/openpnp/machine/reference/driver/LinuxCNC.java
@@ -221,7 +221,7 @@ public class LinuxCNC implements ReferenceDriver, Runnable {
     }
 
     public synchronized void connect(String serverIp, int port) throws Exception {
-        // disconnect();
+        disconnect();
         Logger.debug("connect({}, {})", serverIp, port);
         SocketAddress sa = new InetSocketAddress(serverIp, port);
         socket = new Socket();
@@ -242,7 +242,6 @@ public class LinuxCNC implements ReferenceDriver, Runnable {
             // dump that is sent after reset.
             // responses = sendCommand(null, 3000);
         }
-        connected = true;
 
         responses = sendCommand("hello EMC x 1.1");
         responses.addAll(sendCommand("set enable EMCTOO"));
@@ -259,14 +258,7 @@ public class LinuxCNC implements ReferenceDriver, Runnable {
         // This will be used later to determine the return status.
         responses.addAll(sendCommand("set verbose on"));
 
-        processConnectionResponses(responses);
-
-        if (!connected) {
-            throw new Exception(
-                    "Unable to receive connection response from LinuxCNC ver 1.1. Check your server ip and port in machine.xml");
-        }
-
-        if (!connected) {
+        if (!processConnectionResponses(responses)) {
             throw new Exception(String.format(
                     "Unable to receive connection response from LinuxCNC. Check your server ip and port in machine.xml and that you are running at least version %f of LinuxCNCrsh",
                     minimumRequiredVersion));
@@ -290,18 +282,20 @@ public class LinuxCNC implements ReferenceDriver, Runnable {
         // Reset all axes to 0, in case the firmware was not reset on
         // connect.
         sendCommand("set mdi G92 X0 Y0 Z0 A0");
+        
+        connected = true;
     }
 
-    private void processConnectionResponses(List<String> responses) {
+    private boolean processConnectionResponses(List<String> responses) {
         for (String response : responses) {
             if (response.startsWith("HELLO ACK EMCNETSVR 1.1")) {
-
                 connectedVersion = 1.1;
-                connected = true;
                 Logger.debug(
                         String.format("Connected to LinuxCNCrsh Version: %.2f", connectedVersion));
+                return true;
             }
         }
+        return false;
     }
 
     public synchronized void disconnect() {