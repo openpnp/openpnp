71c342dd42f360706814d2f2ce1769914e2cba38
Scott Wilson <scott.t.wilson@gmail.com>
2020-02-24 17:12:03 -0500

    execute script on actuation

--- src/main/java/org/openpnp/machine/reference/ScriptActuator.java
+++ src/main/java/org/openpnp/machine/reference/ScriptActuator.java
@@ -1,6 +1,7 @@
 package org.openpnp.machine.reference;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.openpnp.Scripting;
@@ -16,8 +17,6 @@ public class ScriptActuator extends ReferenceActuator {
     private void execute(Map<String, Object> globals) throws Exception {
         // Using https://docs.oracle.com/javase/7/docs/technotes/guides/scripting/programmer_guide/ 
         // we should be able to call specific functions, and return a value.
-        
-        
         Scripting scripting = Configuration.get().getScripting();
         File scriptsDirectory = scripting.getScriptsDirectory();
         File script = new File(scriptsDirectory, scriptName);
@@ -26,17 +25,16 @@ public class ScriptActuator extends ReferenceActuator {
     
     @Override
     public void actuate(boolean on) throws Exception {
-        super.actuate(on);
+        Map<String, Object> globals = new HashMap<>();
+        globals.put("actuateBoolean", on);
+        this.execute(globals);
     }
 
     @Override
     public void actuate(double value) throws Exception {
-        super.actuate(value);
-    }
-
-    @Override
-    public String read() throws Exception {
-        return super.read();
+        Map<String, Object> globals = new HashMap<>();
+        globals.put("actuateDouble", value);
+        this.execute(globals);
     }
 
     @Override
