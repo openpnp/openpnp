ab609ecae1052fa8fb29d167a0896a94bced98a4
markmaker <mark@makr.zone>
2020-03-11 22:53:45 +0100

    Straggler

--- src/main/java/org/openpnp/machine/reference/feeder/BlindsFeeder.java
+++ src/main/java/org/openpnp/machine/reference/feeder/BlindsFeeder.java
@@ -31,9 +31,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.swing.Action;
 
