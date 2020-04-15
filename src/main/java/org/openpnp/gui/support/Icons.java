6a619ddbce65db2122726152855beca823a9cb7d
markmaker <mark@makr.zone>
2020-04-15 12:53:28 +0200

    Added a blinking icon to the process instructions displayed underneath the camera views.

--- src/main/java/org/openpnp/gui/support/Icons.java
+++ src/main/java/org/openpnp/gui/support/Icons.java
@@ -83,6 +83,8 @@ public class Icons {
     public static Icon lockQuestion = getIcon("/icons/lock-question.svg");
 
     public static Icon openSCadIcon = getIcon("/icons/openscad-icon.svg");
+    public static Icon processActivity1Icon = getIcon("/icons/process-activity-1.svg");
+    public static Icon processActivity2Icon = getIcon("/icons/process-activity-2.svg");
 
     public static Icon getIcon(String resourceName, int width, int height) {
         if (resourceName.endsWith(".svg")) {
