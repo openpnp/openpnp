de2d6385424f7537d84d078cec29613d39c164a8
Jason von Nieda <jason@vonnieda.org>
2020-04-20 21:21:37 -0500

    Adds several actuatorRead tests, along with a framework for running functional tests with GcodeDriver.

--- src/test/java/GcodeDriverTest.java
+++ src/test/java/GcodeDriverTest.java
@@ -1,22 +1,17 @@
 import java.io.File;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openpnp.machine.reference.ReferenceActuator;
 import org.openpnp.machine.reference.ReferenceMachine;
 import org.openpnp.machine.reference.driver.GcodeDriver;
 import org.openpnp.machine.reference.driver.GcodeDriver.CommandType;
 import org.openpnp.machine.reference.driver.TcpCommunications;
-import org.openpnp.model.Board;
-import org.openpnp.model.Board.Side;
-import org.openpnp.model.BoardLocation;
 import org.openpnp.model.Configuration;
-import org.openpnp.model.Job;
-import org.openpnp.model.LengthUnit;
-import org.openpnp.model.Location;
-import org.openpnp.model.Placement;
 import org.openpnp.spi.Actuator;
 import org.openpnp.spi.Machine;
-import org.openpnp.spi.Nozzle;
 import org.openpnp.util.GcodeServer;
 import org.pmw.tinylog.Configurator;
 import org.pmw.tinylog.Level;
@@ -24,28 +19,28 @@ import org.pmw.tinylog.Level;
 import com.google.common.io.Files;
 
 public class GcodeDriverTest {
-    @Test
-    public void testSimpleJob() throws Exception {
+    GcodeServer server;
+    
+    @Before
+    public void before() throws Exception {
         /**
-         * Configure logging to show everything.
+         * Uncomment this to enable TRACE logging, which will show Gcode commands and responses.
          */
-        Configurator.currentConfig()
-                    .formatPattern("{date:yyyy-MM-dd HH:mm:ss.SSS} {class_name} {level}: {message}")
-                    .level(Level.TRACE)
-                    .activate();
+//        Configurator.currentConfig()
+//                    .formatPattern("{date:yyyy-MM-dd HH:mm:ss.SSS} {class_name} {level}: {message}")
+//                    .level(Level.TRACE)
+//                    .activate();
 
         /**
-         * Set up a TCP based GcodeServer that we'll connect to from GcodeDriver.
+         * Set up a TCP based GcodeServer that we'll connect to from GcodeDriver and create
+         * responses for the connect, enable, and home commands. 
          */
-        GcodeServer server = new GcodeServer();
+        server = new GcodeServer();
         server.addCommandResponse("G21 ; Set millimeters mode", "ok");
         server.addCommandResponse("G90 ; Set absolute positioning mode", "ok");
         server.addCommandResponse("M82 ; Set absolute mode for extruder", "ok");
         server.addCommandResponse("G28 ; Home all axes", "ok");
-        server.addCommandResponse("G0 X10.0000 Y10.0000 Z10.0000 E10.0000 F1000 ; Send standard Gcode move", "ok");
-        server.addCommandResponse("M400 ; Wait for moves to complete before returning", "ok");
-        server.addCommandResponse("READ A1", "read:a1:497\nok");
-        
+
         /**
          * Create a new config directory and load the default configuration.
          */
@@ -56,65 +51,135 @@ public class GcodeDriverTest {
         Configuration.get().load();
 
         /**
-         * Create a GcodeDriver and set it up to connect to the GcodeServer that was just
-         * created.
+         * Create a GcodeDriver and set it up to connect to the GcodeServer that was just created.
+         * We also lower some timeouts to make the tests run faster, as responses on localhost
+         * should be very fast.
          */
-        GcodeDriver gcodeDriver = new GcodeDriver();
-        gcodeDriver.createDefaults();
-        gcodeDriver.setCommunicationsType("tcp");
-        TcpCommunications tcp = (TcpCommunications) gcodeDriver.getCommunications();
+        GcodeDriver driver = new GcodeDriver();
+        driver.createDefaults();
+        driver.setConnectionKeepAlive(false);
+        driver.setCommunicationsType("tcp");
+        TcpCommunications tcp = (TcpCommunications) driver.getCommunications();
         tcp.setIpAddress("localhost");
         tcp.setPort(server.getListenerPort());
-
-        /**
-         * Create an Actuator for reading, and configure the GcodeDriver's command and regex for it.
-         */
-        Machine machine = Configuration.get().getMachine();
-        Actuator actuator = new ReferenceActuator();
-        machine.addActuator(actuator);
-        gcodeDriver.setCommand(actuator, CommandType.ACTUATOR_READ_COMMAND, "READ A1");
-        gcodeDriver.setCommand(actuator, CommandType.ACTUATOR_READ_REGEX, "read:a1:(?<Value>-?\\d+)");
-
+        driver.setConnectWaitTimeMilliseconds(0);
+        driver.setTimeoutMilliseconds(500);
+        
         /**
          * Configure the machine to use the new GcodeDriver and initialize the machine.
          */
-        ReferenceMachine referenceMachine = (ReferenceMachine) machine;
-        referenceMachine.setDriver(gcodeDriver);
+        ReferenceMachine referenceMachine = (ReferenceMachine) Configuration.get().getMachine();
+        referenceMachine.setDriver(driver);
+        
+        /**
+         * Start the machine.
+         */
+        Machine machine = Configuration.get().getMachine();
         machine.setEnabled(true);
         machine.home();
+    }
 
-        /**
-         * Attempt to read the actuator we configured.
-         */
-        actuator.read();
+    @Test
+    public void testActuatorRead() throws Exception {
+        Machine machine = Configuration.get().getMachine();
+        Actuator actuator = new ReferenceActuator();
+        actuator.setName("A1");
+        machine.addActuator(actuator);
+        GcodeDriver driver = (GcodeDriver) ((ReferenceMachine) machine).getDriver();
+        driver.setCommand(actuator, CommandType.ACTUATOR_READ_COMMAND, "READ A1");
+        driver.setCommand(actuator, CommandType.ACTUATOR_READ_REGEX, "read:a1:(?<Value>-?\\d+)");
+
+        server.addCommandResponse("READ A1", "read:a1:497\nok");
         
+        /**
+         * Read the actuator we configured.
+         */
+        Assert.assertEquals(actuator.read(), "497");
+    }
+    
+    @Test
+    public void testActuatorReadNoRegex() throws Exception {
+        Machine machine = Configuration.get().getMachine();
+        Actuator actuator = new ReferenceActuator();
+        actuator.setName("A1");
+        machine.addActuator(actuator);
+        GcodeDriver driver = (GcodeDriver) ((ReferenceMachine) machine).getDriver();
+        driver.setCommand(actuator, CommandType.ACTUATOR_READ_COMMAND, "READ A1");
+
+        server.addCommandResponse("READ A1", "read:a1:497\nok");
+        
+        /**
+         * Attempt to read the actuator we configured. It should throw because we didn't set an
+         * ACTUATOR_READ_REGEX.
+         */
+        try {
+            actuator.read();
+            throw new AssertionError("Expected Actuator.read() to fail because no regex set.");
+        }
+        catch (Exception e) {
+        }
+    }
+    
+    @Test
+    public void testActuatorReadNoCommand() throws Exception {
+        Machine machine = Configuration.get().getMachine();
+        Actuator actuator = new ReferenceActuator();
+        actuator.setName("A1");
+        machine.addActuator(actuator);
+        GcodeDriver driver = (GcodeDriver) ((ReferenceMachine) machine).getDriver();
+        driver.setCommand(actuator, CommandType.ACTUATOR_READ_REGEX, "read:a1:(?<Value>-?\\d+)");
+
+        server.addCommandResponse("READ A1", "read:a1:497\nok");
+        
+        /**
+         * Attempt to read the actuator we configured. It should fail because we didn't set an
+         * ACTUATOR_READ_COMMAND.
+         */
+        try {
+            actuator.read();
+            throw new AssertionError("Expected Actuator.read() to fail because no command set.");
+        }
+        catch (Exception e) {
+        }
+    }
+    
+    @Test
+    public void testActuatorReadBadRegex() throws Exception {
+        Machine machine = Configuration.get().getMachine();
+        Actuator actuator = new ReferenceActuator();
+        actuator.setName("A1");
+        machine.addActuator(actuator);
+        GcodeDriver driver = (GcodeDriver) ((ReferenceMachine) machine).getDriver();
+        driver.setCommand(actuator, CommandType.ACTUATOR_READ_COMMAND, "READ A1");
+        driver.setCommand(actuator, CommandType.ACTUATOR_READ_REGEX, "reXXad:a1:(?<Value>-?\\d+)");
+
+        server.addCommandResponse("READ A1", "read:a1:497\nok");
+        
+        /**
+         * Attempt to read the actuator we configured. It should fail because the 
+         * ACTUATOR_READ_REGEX is incorrect and will not match the response.
+         */
+        try {
+            actuator.read();
+            throw new AssertionError("Expected Actuator.read() to fail because invalid regex set.");
+        }
+        catch (Exception e) {
+        }
+    }
+    
+    @After
+    public void after() throws Exception {
+        /**
+         * TODO: This is cleaner than not shutting it down, but it causes a 3s delay in the test
+         * because the TCP implementation does not handle timeouts correctly. Until that's fixed,
+         * this can be left out to speed up the tests. 
+         */
+//        /**
+//         * Stop the machine.
+//         */
+//        Machine machine = Configuration.get().getMachine();
+//        machine.setEnabled(false);
+
         server.shutdown();
-    }
-
-    private Job createSimpleJob() {
-        Job job = new Job();
-
-        Board board = new Board();
-        board.setName("test");
-
-        board.addPlacement(createPlacement("R1", "R-0805-10K", 10, 10, 0, 45, Side.Top));
-        board.addPlacement(createPlacement("R2", "R-0805-10K", 20, 20, 0, 90, Side.Top));
-
-        BoardLocation boardLocation = new BoardLocation(board);
-        boardLocation.setLocation(new Location(LengthUnit.Millimeters, 0, 0, 0, 0));
-        boardLocation.setSide(Side.Top);
-
-        job.addBoardLocation(boardLocation);
-
-        return job;
-    }
-
-    public static Placement createPlacement(String id, String partId, double x, double y, double z,
-            double rotation, Side side) {
-        Placement placement = new Placement(id);
-        placement.setPart(Configuration.get().getPart(partId));
-        placement.setLocation(new Location(LengthUnit.Millimeters, x, y, z, rotation));
-        placement.setSide(side);
-        return placement;
     }
 }
