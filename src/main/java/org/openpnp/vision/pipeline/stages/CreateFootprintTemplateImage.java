7926d35f2ad4c86c3903b887737c2a9ae9e9d1e5
markmaker <mark@makr.zone>
2020-08-07 03:45:28 +0200

    Footprint template image creation enhanced and refactored (#1032)

--- src/main/java/org/openpnp/vision/pipeline/stages/CreateFootprintTemplateImage.java
+++ src/main/java/org/openpnp/vision/pipeline/stages/CreateFootprintTemplateImage.java
@@ -1,86 +1,97 @@
 package org.openpnp.vision.pipeline.stages;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.Shape;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 import org.openpnp.model.Footprint;
-import org.openpnp.model.Length;
-import org.openpnp.model.Location;
 import org.openpnp.spi.Camera;
 import org.openpnp.util.OpenCvUtils;
 import org.openpnp.vision.pipeline.CvPipeline;
 import org.openpnp.vision.pipeline.CvStage;
+import org.openpnp.vision.pipeline.Property;
 import org.openpnp.vision.pipeline.Stage;
+import org.openpnp.vision.pipeline.stages.convert.ColorConverter;
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.convert.Convert;
 
 @Stage(description="Creates a template from the specified footprint and camera properties. The template is scaled to the camera's units.")
 public class CreateFootprintTemplateImage extends CvStage {
+
+    public enum FootprintView {
+        Fiducial,
+        TopView,
+        BottomView
+    }
+    @Attribute(required=false)
+    @Property(description = "Determines, how the footprint is drawn. Fiducial: only draws the pads, TopView: draws body over pads, "
+            + "BottomView: draws pads over body.")
+    private FootprintView footprintView = FootprintView.Fiducial;
+
+    @Element(required=false)
+    @Convert(ColorConverter.class)
+    @Property(description = "Color of the pads.")
+    private Color padsColor = Color.white; 
+
+    @Element(required=false)
+    @Convert(ColorConverter.class)
+    @Property(description = "Color of the body.")
+    private Color bodyColor = Color.black; 
+
+    @Element(required=false)
+    @Convert(ColorConverter.class)
+    @Property(description = "Color of the background.")
+    private Color backgroundColor = Color.black; 
+
+    public FootprintView getFootprintView() {
+        return footprintView;
+    }
+
+    public void setFootprintView(FootprintView footprintView) {
+        this.footprintView = footprintView;
+    }
+
+    public Color getPadsColor() {
+        return padsColor;
+    }
+
+    public void setPadsColor(Color padsColor) {
+        this.padsColor = padsColor;
+    }
+
+    public Color getBodyColor() {
+        return bodyColor;
+    }
+
+    public void setBodyColor(Color bodyColor) {
+        this.bodyColor = bodyColor;
+    }
+
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(Color backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
     @Override
     public Result process(CvPipeline pipeline) throws Exception {
         Camera camera = (Camera) pipeline.getProperty("camera");
         Footprint footprint = (Footprint) pipeline.getProperty("footprint");
-        
+
         if (camera == null) {
             throw new Exception("Property \"camera\" is required.");
         }
         if (footprint == null) {
             throw new Exception("Property \"footprint\" is required.");
         }
-        
-        Location unitsPerPixel = camera.getUnitsPerPixel();
-        
-        Shape shape = footprint.getShape();
 
-        if (shape == null) {
-            throw new Exception(
-                    "Invalid footprint found, unable to create template for fiducial match. See https://github.com/openpnp/openpnp/wiki/Fiducials.");
-        }
-
-        // Determine the scaling factor to go from Outline units to
-        // Camera units.
-        Length l = new Length(1, footprint.getUnits());
-        l = l.convertToUnits(unitsPerPixel.getUnits());
-        double unitScale = l.getValue();
-
-        // Create a transform to scale the Shape by
-        AffineTransform tx = new AffineTransform();
-
-        // First we scale by units to convert the units and then we scale
-        // by the camera X and Y units per pixels to get pixel locations.
-        tx.scale(unitScale, unitScale);
-        tx.scale(1.0 / unitsPerPixel.getX(), 1.0 / unitsPerPixel.getY());
-
-        // Transform the Shape and draw it out.
-        shape = tx.createTransformedShape(shape);
-
-        Rectangle2D bounds = shape.getBounds2D();
-
-        if (bounds.getWidth() == 0 || bounds.getHeight() == 0) {
-            throw new Exception(
-                    "Invalid footprint found, unable to create template for fiducial match. Width and height of pads must be greater than 0. See https://github.com/openpnp/openpnp/wiki/Fiducials.");
-        }
-
-        // Make the image 50% bigger than the shape. This gives better
-        // recognition performance because it allows some border around the edges.
-        double width = bounds.getWidth() * 1.5;
-        double height = bounds.getHeight() * 1.5;
-        BufferedImage template =
-                new BufferedImage((int) width, (int) height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = (Graphics2D) template.getGraphics();
-
-        g2d.setStroke(new BasicStroke(1f));
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setColor(Color.white);
-        // center the drawing
-        g2d.translate(width / 2, height / 2);
-        g2d.fill(shape);
-
-        g2d.dispose();
+        BufferedImage template = OpenCvUtils.createFootprintTemplate(camera, footprint, 0.0,
+                footprintView == FootprintView.TopView, 
+                padsColor, 
+                (footprintView == FootprintView.Fiducial ? null : bodyColor), 
+                backgroundColor, 1.5, 3);
 
         return new Result(OpenCvUtils.toMat(template));
     }
