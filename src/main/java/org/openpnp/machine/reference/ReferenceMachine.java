e1f6010d57045353cbfd005cf1f1c4299a82171f
Jason von Nieda <jason@vonnieda.org>
2020-08-20 12:31:04 -0500

    Revert "Add Gstreamer Camera (#1023)" (#1041)
    This reverts commit 6da40f9141badb27efd79c1faef88f436fda2dea.

--- src/main/java/org/openpnp/machine/reference/ReferenceMachine.java
+++ src/main/java/org/openpnp/machine/reference/ReferenceMachine.java
@@ -33,7 +33,6 @@ import org.openpnp.gui.support.Wizard;
 import org.openpnp.machine.marek.MarekNozzle;
 import org.openpnp.machine.neoden4.Neoden4Camera;
 import org.openpnp.machine.rapidplacer.RapidFeeder;
-import org.openpnp.machine.reference.camera.GstreamerCamera;
 import org.openpnp.machine.reference.camera.ImageCamera;
 import org.openpnp.machine.reference.camera.OnvifIPCamera;
 import org.openpnp.machine.reference.camera.OpenCvCamera;
@@ -247,7 +246,6 @@ public class ReferenceMachine extends AbstractMachine {
         l.add(ImageCamera.class);
         l.add(SwitcherCamera.class);
         l.add(SimulatedUpCamera.class);
-        l.add(GstreamerCamera.class);
         return l;
     }
 
