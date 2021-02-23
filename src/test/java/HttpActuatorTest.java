cee11e7c021616304d86cf3e4aa31395b90c7e93
Thorsten Wilmer <wilmer.thorsten@gmail.com>
2021-02-23 17:41:59 +0100

    Add a unit test to HttpActuator read
    The unit test provides a sample web server (built in from java)
    and provides one url with a value and the value is checked that
    it matched the regex.

--- src/test/java/HttpActuatorTest.java
+++ src/test/java/HttpActuatorTest.java
@@ -1,7 +1,8 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 import javax.swing.Action;
 import javax.swing.Icon;
@@ -11,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openpnp.CameraListener;
 import org.openpnp.gui.support.Wizard;
+import org.openpnp.machine.reference.HttpActuator;
 import org.openpnp.model.Configuration;
 import org.openpnp.model.Length;
 import org.openpnp.model.LengthUnit;
@@ -25,9 +27,12 @@ import org.openpnp.spi.base.AbstractHeadMountable;
 import org.openpnp.util.VisionUtils;
 
 import com.google.common.io.Files;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 
-public class VisionUtilsTest {
+public class HttpActuatorTest {
 	
 	@Before
 	public void before() throws Exception {
@@ -45,225 +50,58 @@ public class VisionUtilsTest {
 	 
     @Test
     public void testOffsets() {
-        Camera camera = new TestCamera();
-        Location location = camera.getLocation();
-        Assert.assertEquals(location, new Location(LengthUnit.Millimeters, 0, 0, 0, 0));
-        Assert.assertEquals(camera.getWidth(), 640);
-        Assert.assertEquals(camera.getHeight(), 480);
-        Location pixelOffsets = VisionUtils.getPixelCenterOffsets(camera, 100, 100);
-        Assert.assertEquals(pixelOffsets, new Location(LengthUnit.Millimeters, -220, 140, 0, 0));
-        Location pixelLocation = VisionUtils.getPixelLocation(camera, 100, 100);
-        Assert.assertEquals(pixelLocation, new Location(LengthUnit.Millimeters, -220, 140, 0, 0));
+        TestHttpServer server=new TestHttpServer ();
+        HttpActuator actuator=new HttpActuator();
+        
+        
+        actuator.setRegex("read:(?<Value>-?\\d+)");
+        actuator.setReadUrl("http://127.0.0.1:3042/msr");
+        
+        String stringResult="";
+        try {
+            Configuration.get().getMachine().setEnabled(true);
+            stringResult= Configuration.get().getMachine().execute(() -> {
+                 return  actuator.read();
+            }, false, 0);
+      
+        }
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        Double result=Double.parseDouble(stringResult);
+        
+        Assert.assertEquals(result, Double.valueOf( 42.0));
+       
     }
-
-    static class TestCamera extends AbstractHeadMountable implements Camera {
-        protected Head head;
-
-        @Override
-        public String getId() {
-            return null;
+    static class TestHttpServer   {
+        TestHttpServer() {
+            HttpServer server;
+            try {
+                server = HttpServer.create(new InetSocketAddress(3042), 0);
+                server.createContext("/msr", new MyHandler());
+                server.setExecutor(null); // creates a default executor
+                server.start();  
+            }
+            catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+          
         }
-
-        @Override
-        public Head getHead() {
-            return head;
-        }
-
-        @Override
-        public void setHead(Head head) {
-            this.head = head;
-        }
-
-        @Override
-        public Location getLocation() {
-            return new Location(LengthUnit.Millimeters, 0, 0, 0, 0);
-        }
-
-        @Override
-        public Location getLocation(HeadMountable tool) {
-            if (tool != null) {
-                return getLocation().subtract(tool.getCameraToolCalibratedOffset(this));
+        static class MyHandler implements HttpHandler {
+            @Override
+            public void handle(HttpExchange t) throws IOException {
+                String response = "read:42";
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
             }
 
-            return getLocation();
-        }
-
-       @Override
-        public Location getCameraToolCalibratedOffset(Camera camera) {
-            return new Location(camera.getUnitsPerPixel().getUnits());
-        }
-
-        @Override
-        public Wizard getConfigurationWizard() {
-            return null;
-        }
-
-        @Override
-        public String getPropertySheetHolderTitle() {
-            return null;
-        }
-
-        @Override
-        public PropertySheetHolder[] getChildPropertySheetHolders() {
-            return null;
-        }
-
-        @Override
-        public PropertySheet[] getPropertySheets() {
-            return null;
-        }
-
-        @Override
-        public Action[] getPropertySheetHolderActions() {
-            return null;
-        }
-
-        @Override
-        public String getName() {
-            return null;
-        }
-
-        @Override
-        public void setName(String name) {
-
-        }
-
-        @Override
-        public Looking getLooking() {
-            return null;
-        }
-
-        @Override
-        public void setLooking(Looking looking) {
-
-        }
-
-        @Override
-        public Location getUnitsPerPixel() {
-            return new Location(LengthUnit.Millimeters, 1, 1, 0, 0);
-        }
-
-        @Override
-        public void setUnitsPerPixel(Location unitsPerPixel) {
-
-        }
-
-        @Override
-        public BufferedImage capture() {
-            return null;
-        }
-
-        @Override
-        public BufferedImage captureTransformed() {
-            return null;
-        }
-
-        @Override
-        public BufferedImage captureRaw() {
-            return null;
-        }
-
-        @Override
-        public void startContinuousCapture(CameraListener listener) {
-
-        }
-
-        @Override
-        public void stopContinuousCapture(CameraListener listener) {
-
-        }
-
-        @Override
-        public void setVisionProvider(VisionProvider visionProvider) {
-
-        }
-
-        @Override
-        public VisionProvider getVisionProvider() {
-            return null;
-        }
-
-        @Override
-        public int getWidth() {
-            return 640;
-        }
-
-        @Override
-        public int getHeight() {
-            return 480;
-        }
-
-        @Override
-        public Icon getPropertySheetHolderIcon() {
-            return null;
-        }
-
-        @Override
-        public void close() throws IOException {
-        }
-
-        @Override
-        public BufferedImage settleAndCapture() throws Exception {
-            return null;
-        }
-
-        @Override
-        public BufferedImage lightSettleAndCapture() {
-            return null;
-        }
-
-        @Override
-        public void actuateLightBeforeCapture(Object light) throws Exception {
-        }
-
-        @Override
-        public void actuateLightAfterCapture() throws Exception {
-        }
-
-        @Override
-        public Length getSafeZ() {
-            return null;
-        }
-
-        @Override
-        public Location getHeadOffsets() {
-            return null;
-        }
-
-        @Override
-        public void setHeadOffsets(Location headOffsets) {
-        }
-
-        @Override
-        public void home() throws Exception {
-        }
-
-        @Override
-        public Actuator getLightActuator() {
-            return null;
-        }
-
-        @Override
-        public void ensureCameraVisible() {
-        }
-
-        @Override
-        public boolean hasNewFrame() {
-            return true;
-        }
-
-        public Location getUnitsPerPixel(Length z) {
-            return new Location(LengthUnit.Millimeters, 1, 1, 0, 0).derive(null, null, z.getValue(), null);
-        }
-
-        @Override
-        public Length getDefaultZ() {
-            return new Length(0.0, LengthUnit.Millimeters);
-        }
-
-        @Override
-        public boolean isShownInMultiCameraView() {
-            return false;
+           
         }
     }
+
 }
