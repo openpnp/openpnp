ee55dca2148d1429ee0c59a4769a29a5721dddc8
Justin Nesselrotte <admin@jnesselr.org>
2021-04-18 10:56:24 -0600

    Added IndexProperties class

---
+++ src/main/java/org/openpnp/machine/index/IndexProperties.java
@@ -0,0 +1,11 @@
+package org.openpnp.machine.index;
+
+import org.openpnp.spi.Machine;
+
+public class IndexProperties {
+    private final Machine machine;
+
+    public IndexProperties(Machine machine) {
+        this.machine = machine;
+    }
+}
