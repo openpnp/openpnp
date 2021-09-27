dad2c2537c188b4f72e111dd880f1e725e2dd409
Nikola <karlikova.nikola@gmail.com>
2021-09-27 21:13:51 +0200

    Add Toolbar to Vision panel with Add, Remove, Copy and Paste buttons

--- src/main/java/org/openpnp/gui/PipelinesPanel.java
+++ src/main/java/org/openpnp/gui/PipelinesPanel.java
@@ -9,6 +9,8 @@ import org.openpnp.model.Pipeline;
 import org.openpnp.spi.PartAlignment;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.TableRowSorter;
@@ -34,17 +36,17 @@ public class PipelinesPanel extends JPanel implements WizardContainer {
 
     public PipelinesPanel(Configuration configuration, Frame frame) {
 
-        singleSelectionActionGroup = new ActionGroup(deletePipelineAction, pickPipelineAction, copyPipelineToClipboardAction);
+        singleSelectionActionGroup = new ActionGroup(deletePipelineAction, pastePipelineFromClipboardAction, copyPipelineToClipboardAction);
         singleSelectionActionGroup.setEnabled(false);
         multiSelectionActionGroup = new ActionGroup(deletePipelineAction);
         multiSelectionActionGroup.setEnabled(false);
 
         setLayout(new BorderLayout(0, 0));
+
+        createAndAddToolbar();
+
         tableModel = new PipelinesTableModel();
         tableSorter = new TableRowSorter<>(tableModel);
-
-//        JComboBox pipelinesCombo = new JComboBox(new PackagesComboBoxModel<Pipeline>());
-//        pipelinesCombo.setRenderer(new IdentifiableListCellRenderer<Pipeline>());
 
         JSplitPane splitPane = new JSplitPane();
         splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
@@ -63,58 +65,77 @@ public class PipelinesPanel extends JPanel implements WizardContainer {
 
         table = new AutoSelectTextTable(tableModel);
         table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-//        table.setDefaultEditor(org.openpnp.model.Package.class,
-//                new DefaultCellEditor(pipelinesCombo));
-//        table.setDefaultRenderer(org.openpnp.model.Package.class,
-//                new IdentifiableTableCellRenderer<Package>());
 
         table.setRowSorter(tableSorter);
         table.getTableHeader().setDefaultRenderer(new MultisortTableHeaderCellRenderer());
         splitPane.setLeftComponent(new JScrollPane(table));
         splitPane.setRightComponent(tabbedPane);
 
-        table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                if (e.getValueIsAdjusting()) {
-                    return;
-                }
-
-                List<Pipeline> selections = getSelections();
-
-                if (selections.size() > 1) {
-                    singleSelectionActionGroup.setEnabled(false);
-                    multiSelectionActionGroup.setEnabled(true);
-                } else {
-                    multiSelectionActionGroup.setEnabled(false);
-                    singleSelectionActionGroup.setEnabled(!selections.isEmpty());
-                }
-
-                Pipeline pipeline = getSelection();
-
-                int selectedTab = tabbedPane.getSelectedIndex();
-                tabbedPane.removeAll();
-
-                if (pipeline != null) {
-                    PartAlignment vision = Configuration.get().getMachine().getPartAlignments().get(0);
-                    Wizard wizard = vision.getPipelineConfigurationWizard(pipeline);
-                    if (wizard != null) {
-                        JPanel panel = new JPanel();
-                        panel.setLayout(new BorderLayout());
-                        panel.add(wizard.getWizardPanel());
-                        tabbedPane.add(wizard.getWizardName(), new JScrollPane(panel));
-                        wizard.setWizardContainer(PipelinesPanel.this);
-                    }
-                }
-
-                if (selectedTab >= 0 && selectedTab < tabbedPane.getTabCount()) {
-                    tabbedPane.setSelectedIndex(selectedTab);
-                }
-
-                revalidate();
-                repaint();
+        table.getSelectionModel().addListSelectionListener(e -> {
+            if (e.getValueIsAdjusting()) {
+                return;
             }
+
+            List<Pipeline> selections = getSelections();
+
+            if (selections.size() > 1) {
+                singleSelectionActionGroup.setEnabled(false);
+                multiSelectionActionGroup.setEnabled(true);
+            } else {
+                multiSelectionActionGroup.setEnabled(false);
+                singleSelectionActionGroup.setEnabled(!selections.isEmpty());
+            }
+
+            Pipeline pipeline = getSelection();
+
+            int selectedTab = tabbedPane.getSelectedIndex();
+            tabbedPane.removeAll();
+
+            if (pipeline != null) {
+                PartAlignment vision = Configuration.get().getMachine().getPartAlignments().get(0);
+                Wizard wizard = vision.getPipelineConfigurationWizard(pipeline);
+                if (wizard != null) {
+                    JPanel panel = new JPanel();
+                    panel.setLayout(new BorderLayout());
+                    panel.add(wizard.getWizardPanel());
+                    tabbedPane.add(wizard.getWizardName(), new JScrollPane(panel));
+                    wizard.setWizardContainer(PipelinesPanel.this);
+                }
+            }
+
+            if (selectedTab >= 0 && selectedTab < tabbedPane.getTabCount()) {
+                tabbedPane.setSelectedIndex(selectedTab);
+            }
+
+            revalidate();
+            repaint();
         });
+    }
+
+    private void createAndAddToolbar() {
+        JPanel toolbarPanel = new JPanel();
+        add(toolbarPanel, BorderLayout.NORTH);
+        toolbarPanel.setLayout(new BorderLayout(0, 0));
+
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(false);
+        toolbarPanel.add(toolBar);
+
+        JPanel upperPanel = new JPanel();
+        toolbarPanel.add(upperPanel, BorderLayout.EAST);
+
+        toolBar.add(newPipelineAction);
+        toolBar.add(deletePipelineAction);
+
+        toolBar.addSeparator();
+
+        JButton copyToClipboardButton = new JButton(copyPipelineToClipboardAction);
+        copyToClipboardButton.setHideActionText(true);
+        toolBar.add(copyToClipboardButton);
+
+        JButton pasteFromClipboardButton = new JButton(pastePipelineFromClipboardAction);
+        pasteFromClipboardButton.setHideActionText(true);
+        toolBar.add(pasteFromClipboardButton);
     }
 
     private Pipeline getSelection() {
@@ -134,14 +155,26 @@ public class PipelinesPanel extends JPanel implements WizardContainer {
         return selections;
     }
 
-    public final Action deletePipelineAction = new AbstractAction() {
+    public final Action newPipelineAction = new AbstractAction() {
+        {
+            putValue(SMALL_ICON, Icons.add);
+            putValue(NAME, "New Pipeline...");
+            putValue(SHORT_DESCRIPTION, "Create a new pipeline, specifying it's ID.");
+        }
+
         @Override
         public void actionPerformed(ActionEvent e) {
 
         }
     };
 
-    public final Action pickPipelineAction = new AbstractAction() {
+    public final Action deletePipelineAction = new AbstractAction() {
+        {
+            putValue(SMALL_ICON, Icons.delete);
+            putValue(NAME, "Delete Pipeline");
+            putValue(SHORT_DESCRIPTION, "Delete the currently selected pipeline.");
+        }
+
         @Override
         public void actionPerformed(ActionEvent e) {
 
@@ -149,6 +182,26 @@ public class PipelinesPanel extends JPanel implements WizardContainer {
     };
 
     public final Action copyPipelineToClipboardAction = new AbstractAction() {
+        {
+            putValue(SMALL_ICON, Icons.copy);
+            putValue(NAME, "Copy Pipeline to Clipboard");
+            putValue(SHORT_DESCRIPTION,
+                    "Copy the currently selected pipeline to the clipboard in text format.");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    };
+
+    public final Action pastePipelineFromClipboardAction = new AbstractAction() {
+        {
+            putValue(SMALL_ICON, Icons.paste);
+            putValue(NAME, "Create Pipeline from Clipboard");
+            putValue(SHORT_DESCRIPTION, "Create a new pipeline from a definition on the clipboard.");
+        }
+
         @Override
         public void actionPerformed(ActionEvent e) {
 
