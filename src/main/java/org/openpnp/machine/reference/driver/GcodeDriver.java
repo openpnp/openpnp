2b181b1149fbc268d9b4cb416ff82d77e890ef4f
Jan <99149230+janm012012@users.noreply.github.com>
2024-02-21 22:36:34 +0100

    typo fixed

--- src/main/java/org/openpnp/machine/reference/driver/GcodeDriver.java
+++ src/main/java/org/openpnp/machine/reference/driver/GcodeDriver.java
@@ -451,7 +451,7 @@ public class GcodeDriver extends AbstractReferenceDriver implements Named {
         getSendOnChangeFeedRate().reset();
         getSendOnChangeAcceleration().reset();
         getSendOnChangeJerk().reset();
-        Logger.trace("Send-on-change logic for FeedRate, Acceleration and Jerk resetted");
+        Logger.trace("Send-on-change logic for Feed Rate, Acceleration and Jerk reset");
     }
     private String sendOnChangeSubstituteAllVariables(String command, Double feedRate, Double acceleration, Double jerk) {
         command = getSendOnChangeFeedRate().substituteVariable(command, feedRate);
