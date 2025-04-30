3b477b12140cd01186328fbafc6652d687f4ca31
Jay <jay@rocket.lan>
2025-05-01 00:31:28 +1000

    pad import corrected to also work with newer mod files

--- src/main/java/org/openpnp/gui/importer/KicadModImporter.java
+++ src/main/java/org/openpnp/gui/importer/KicadModImporter.java
@@ -20,10 +20,12 @@
 package org.openpnp.gui.importer;
 
 import java.awt.FileDialog;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FilenameFilter;
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -36,21 +38,55 @@ import org.openpnp.model.Footprint.Pad;
 /**
  * @author Jonas Lehmke <jonas@lehmke.xyz>
  * 
- * This module reads a KiCad footprint from file (*.kicad_mod) and parses it to a Footprint instance.
- * Rectangular, rounded rectangular, circular and oval pad shapes are supported. Trapezoid and custom 
- * pad shapes are ignored. A FileDialog is openened to select a file once this class is instantiated. 
- * Imported pads are available as List<Pad>.
+ *         This module reads a KiCad footprint from file (*.kicad_mod) and
+ *         parses it to a Footprint instance.
+ *         Rectangular, rounded rectangular, circular and oval pad shapes are
+ *         supported. Trapezoid and custom
+ *         pad shapes are ignored. A FileDialog is openened to select a file
+ *         once this class is instantiated.
+ *         Imported pads are available as List<Pad>.
  * 
- * This module may become part of an all-in-one KiCad board import one day.
+ *         This module may become part of an all-in-one KiCad board import one
+ *         day.
  */
 
- 
 public class KicadModImporter {
 
     Footprint footprint = new Footprint();
 
+    public class PadParserMultiline {
+
+        public List<String> parsePadOccurrencesMultiline(String text) {
+            List<String> capturedContents = new ArrayList<>();
+            Pattern pattern = Pattern.compile("\\(pad(?<content>.*?)\\)", Pattern.DOTALL | Pattern.MULTILINE);
+            Matcher matcher = pattern.matcher(text);
+
+            while (matcher.find()) {
+                // String content = matcher.group("content");
+                int openCount = 1; // Start with 1 because of the initial '(' in "(pad"
+                int closeCount = 0;
+                int startIndex = matcher.start();
+
+                for (int i = startIndex + 4; i < text.length(); i++) {
+                    char currentChar = text.charAt(i);
+                    if (currentChar == '(') {
+                        openCount++;
+                    } else if (currentChar == ')') {
+                        closeCount++;
+                    }
+
+                    if (openCount == closeCount) {
+                        capturedContents.add(text.substring(startIndex + 4, i));
+                        break;
+                    }
+                }
+            }
+            return capturedContents;
+        }
+    }
+
     public class KicadPad {
-    
+
         String padDefinition;
 
         public KicadPad(String definition) {
@@ -58,72 +94,80 @@ public class KicadModImporter {
         }
 
         String getName() {
-            Pattern p = Pattern.compile("^\\(pad\\s\"?(\\w*)\"?\\s(\\w*)\\s(\\w*)");
+            Pattern p = Pattern.compile("\\s*\\t*\"(\\w*)\"\\s*\\t*(\\w*)\\s*\\t*(\\w*)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 return m.group(1);
             }
             return "";
         }
 
         String getType() {
-            Pattern p = Pattern.compile("^\\(pad\\s\"?(\\w*)\"?\\s(\\w*)\\s(\\w*)");
+            Pattern p = Pattern.compile("\\s*\\t*\"(\\w*)\"\\s*\\t*(\\w*)\\s*\\t*(\\w*)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 return m.group(2);
             }
             return "";
         }
 
         String getShape() {
-            Pattern p = Pattern.compile("^\\(pad\\s\"?(\\w*)\"?\\s(\\w*)\\s(\\w*)");
+            Pattern p = Pattern.compile("\\s*\\t*\"(\\w*)\"\\s*\\t*(\\w*)\\s*\\t*(\\w*)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 return m.group(3);
             }
             return "";
         }
 
         double getWidth() {
-            Pattern p = Pattern.compile("\\(size ([\\-0-9.]*) ([\\-0-9\\.]*)\\)");
+            Pattern p = Pattern.compile("\\s*\\t*\\(size\\s*\\t*([\\d\\.+-]*)\\s*\\t*([\\d\\.+-]*)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 return Double.parseDouble(m.group(1));
             }
             return 0.;
         }
 
         double getHeight() {
-            Pattern p = Pattern.compile("\\(size ([\\-0-9\\.]*) ([\\-0-9\\.]*)\\)");
+            Pattern p = Pattern.compile("\\s*\\t*\\(size\\s*\\t*([\\d\\.+-]*)\\s*\\t*([\\d\\.+-]*)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 return Double.parseDouble(m.group(2));
             }
             return 0.;
         }
 
         double getX() {
-            Pattern p = Pattern.compile("\\(at ([\\-0-9.]*) ([\\-0-9.]*)\\s?([^\\)]*)\\)");
+            Pattern p = Pattern.compile("\\s*\\t*\\(at\\s*\\t*([\\d\\.+-]*)\\s*\\t*([\\d\\.+-]*)\\s*\\t*([\\d\\.+-]*)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 return Double.parseDouble(m.group(1));
             }
             return 0.;
         }
 
         double getY() {
-            Pattern p = Pattern.compile("\\(at ([\\-0-9.]*) ([\\-0-9.]*)\\s?([^\\)]*)\\)");
+            Pattern p = Pattern.compile("\\s*\\t*\\(at\\s*\\t*([\\d\\.+-]*)\\s*\\t*([\\d\\.+-]*)\\s*\\t*([\\d\\.+-]*)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 return Double.parseDouble(m.group(2)) * (-1); // Negative because of different conventions
             }
             return 0.;
         }
 
         double getRotation() {
-            Pattern p = Pattern.compile("\\(at ([\\-0-9.]*) ([\\-0-9.]*)\\s?([^\\)]*)\\)");
+            Pattern p = Pattern.compile("\\s*\\t*\\(at\\s*\\t*([\\d\\.+-]*)\\s*\\t*([\\d\\.+-]*)\\s*\\t*([\\d\\.+-]*)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 if (m.group(3).length() > 0) {
                     return Double.parseDouble(m.group(3));
                 }
@@ -132,18 +176,20 @@ public class KicadModImporter {
         }
 
         double getRoundness() {
-            Pattern p = Pattern.compile("\\(roundrect_rratio ([\\-0-9.]*)\\)");
+            Pattern p = Pattern.compile("\\s*\\t*\\(roundrect_rratio\\s*\\t*([\\d\\.+-]*)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 return Double.parseDouble(m.group(1)) * 100;
             }
             return 0.;
         }
 
         boolean isTopCu() {
-            Pattern p = Pattern.compile("\\(layers ([^\\)]*)\\)");
+            Pattern p = Pattern.compile("\\s*\\t*\\(layers\\s*\\t*([^\\)]*)\\s*\\t*\\)",
+                    Pattern.DOTALL | Pattern.MULTILINE);
             Matcher m = p.matcher(padDefinition);
-            if(m.find()) {
+            if (m.find()) {
                 return m.group(1).contains("F.Cu") || m.group(1).contains("*.Cu");
             }
             return false;
@@ -164,46 +210,42 @@ public class KicadModImporter {
                 return;
             }
 
-            File file = new File(new File(fileDialog.getDirectory()), fileDialog.getFile());
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            
-            String line = reader.readLine();
-            while (line != null) {
-                if (line.trim().startsWith("(pad ")) {
-                    KicadPad kipad = new KicadPad(line.trim());
-                    if (kipad.getType().equals("smd") && kipad.isTopCu()) {
-                        Pad pad = new Pad();
-                        pad.setName(kipad.getName());
-                        pad.setWidth(kipad.getWidth());
-                        pad.setHeight(kipad.getHeight());
-                        pad.setX(kipad.getX());
-                        pad.setY(kipad.getY());
-                        pad.setRotation(kipad.getRotation());
+            Path path = Paths.get(fileDialog.getDirectory() + fileDialog.getFile());
+            String content = Files.readString(path);
 
-                        if (kipad.getShape().equals("rect")) {
-                            pad.setRoundness(0);
-                        } else if (kipad.getShape().equals("circle")) {
-                            pad.setRoundness(100);
-                        } else if (kipad.getShape().equals("oval")) {
-                            pad.setRoundness(100);
-                        } else if (kipad.getShape().equals("roundrect")) {
-                            pad.setRoundness(kipad.getRoundness());
-                        } else {
-                            System.out.println("Warning: Unsupported pad type: " + kipad.getShape());
-                            line = reader.readLine();
-                            continue;
-                        }
+            // find all the pads
+            PadParserMultiline parserMultiline = new PadParserMultiline();
+            List<String> padList = parserMultiline.parsePadOccurrencesMultiline(content);
 
-                        footprint.addPad(pad);
+            for (String string : padList) {
+                KicadPad kipad = new KicadPad(string);
+                if (kipad.getType().equals("smd") && kipad.isTopCu()) {
+                    Pad pad = new Pad();
+                    pad.setName(kipad.getName());
+                    pad.setWidth(kipad.getWidth());
+                    pad.setHeight(kipad.getHeight());
+                    pad.setX(kipad.getX());
+                    pad.setY(kipad.getY());
+                    pad.setRotation(kipad.getRotation());
+
+                    if (kipad.getShape().equals("rect")) {
+                        pad.setRoundness(0);
+                    } else if (kipad.getShape().equals("circle")) {
+                        pad.setRoundness(100);
+                    } else if (kipad.getShape().equals("oval")) {
+                        pad.setRoundness(100);
+                    } else if (kipad.getShape().equals("roundrect")) {
+                        pad.setRoundness(kipad.getRoundness());
+                    } else {
+                        System.out.println("Warning: Unsupported pad type: " + kipad.getShape());
+                        continue;
                     }
-                }
 
-                line = reader.readLine();
+                    footprint.addPad(pad);
+                }
             }
 
-            reader.close();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             throw new Exception(Translations.getString("KicadModImporter.LoadFile.Fail") + e.getMessage()); //$NON-NLS-1$
         }
     }
