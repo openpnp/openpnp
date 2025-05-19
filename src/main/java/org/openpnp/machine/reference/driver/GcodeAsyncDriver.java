a6bee5c5243d1e9416054143e5d17a17b273912d
Oo饿不死oO <kuroky.rasum@gmail.com>
2025-05-19 08:11:54 +0800

    Follow the translations.properties-normalised
    Make the translationsroperties file in a normalised format by copying translations.properties-normalised.

--- src/main/java/org/openpnp/machine/reference/driver/GcodeAsyncDriver.java
+++ src/main/java/org/openpnp/machine/reference/driver/GcodeAsyncDriver.java
@@ -417,7 +417,7 @@ public class GcodeAsyncDriver extends GcodeDriver {
     @Override
     public PropertySheet[] getPropertySheets() {
         return Collect.concat(super.getPropertySheets(), new PropertySheet[] { 
-                new PropertySheetWizardAdapter(new GcodeAsyncDriverSettings(this), Translations.getString("GCodeAsyncDriver.AdvancedSettingsWizard.title")) //$NON-NLS-1$
+                new PropertySheetWizardAdapter(new GcodeAsyncDriverSettings(this), Translations.getString("GCodeAsyncDriver.AdvancedSettings.title")) //$NON-NLS-1$
         });
     }
 }
