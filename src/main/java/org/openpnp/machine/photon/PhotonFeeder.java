da80fcfd8b2f452eb1dc27c3c87b40387b45a79c
Jorropo <jorropo.pgm@gmail.com>
2025-09-02 20:36:22 +0200

    photonfeeder: while doing moveWhileFeeding move to safeZ rather than 0
    This fixes an interesting case where my nozzles go above safeZ
    while doing a moveWhileFeeding action.
    
    At first I wanted to move a couple mms above the pick location but
    this is has a lot of tiny details to get right along of being bugged,
    so this makes more sense.

--- src/main/java/org/openpnp/machine/photon/PhotonFeeder.java
+++ src/main/java/org/openpnp/machine/photon/PhotonFeeder.java
@@ -296,7 +296,7 @@ public class PhotonFeeder extends ReferenceFeeder {
                 Thread.sleep(50); // MAGIC: this feels like a good number, there is no particular reason it is this way.
 
                 if (j == 0 && nozzle != null && getMoveWhileFeeding()) {
-                    MovableUtils.moveToLocationAtSafeZ(nozzle, getPickLocation().derive(null, null, Double.NaN, null));
+                    MovableUtils.moveToLocationAtSafeZ(nozzle, getPickLocation().deriveLengths(null, null, nozzle.getEffectiveSafeZ(), null));
                 }
 
                 MoveFeedStatus moveFeedStatus = new MoveFeedStatus(slotAddress);
